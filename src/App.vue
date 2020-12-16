4321d6e40c6eab5a38b8bf5fb6944be6f53c0a14
Jenny Gebske <jenny@ctrl.alt.coop>
2020-12-16 12:56:37 +0100

    adjust font family

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,14 @@
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+
+  html, body {
+    font-family: 'Roboto', Arial, Helvetica, Geneva, sans-serif;
+  }
+
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Roboto', Arial, Helvetica, Geneva, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
