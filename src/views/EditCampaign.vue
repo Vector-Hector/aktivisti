0aa8375dc2e8a61bc448133aae3cbe93161034dd
Jenny Gebske <jenny@ctrl.alt.coop>
2021-01-05 14:17:19 +0100

    fix campaign selection

--- src/views/EditCampaign.vue
+++ src/views/EditCampaign.vue
@@ -26,7 +26,7 @@
         <div class="p-field p-grid">
             <label for="campaign" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">Typ</label>
             <div class="p-col-12 p-md-10">
-                <Dropdown v-model="selectedCampaign.name" :options="campaignTypes" optionLabel="name" placeholder="Wähle einen Kampagnen-Typ aus" />
+                <Dropdown v-model="campaign.type" :options="campaignTypes" optionLabel="name" placeholder="Wähle einen Kampagnen-Typ aus" />
             </div>
         </div>
 
