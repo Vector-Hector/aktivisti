8801c3b1e8d0f0d906967941fe4af0cc2840c1a1
Jenny Gebske <jenny@ctrl.alt.coop>
2021-01-06 12:51:43 +0100

    add map to home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,69 @@
 <template>
-    <h1>Home</h1>
+    <h1>Finde Veranstaltungen in deiner Nähe</h1>
+
+    <div style="height: 75vh; width: 50vw;">
+        <l-map
+            v-model="zoom"
+            :zoom="zoom"
+            :center="center"
+        >
+        <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+
+        <l-control-layers />
+
+        <l-marker :lat-lng="[53, 12]">
+            <l-tooltip>
+                Test Tooltip Text
+            </l-tooltip>
+        </l-marker>
+
+        <l-marker :lat-lng="[52, 13]">
+            <l-popup>
+                Test Popup Text
+            </l-popup>
+        </l-marker>
+
+        </l-map>
+    </div>
 </template>
+
+<script lang="ts">
+    import "leaflet/dist/leaflet.css"
+    import {
+        LMap,
+        LTileLayer,
+        LMarker,
+        LControlLayers,
+        LTooltip,
+        LPopup,
+    } from "@vue-leaflet/vue-leaflet"
+
+    export default {
+        components: {
+            LMap,
+            LTileLayer,
+            LMarker,
+            LControlLayers,
+            LTooltip,
+            LPopup,
+        },
+        data() {
+            return {
+                zoom: 6,
+                iconWidth: 25,
+                iconHeight: 40,
+                center: [51.5, 10],
+            };
+        },
+        computed: {
+        },
+        methods: {
+        },
+    }
+
+</script>
 
 <style scoped>
 </style>
