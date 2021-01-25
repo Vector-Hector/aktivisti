0f2da07593552703e35bc06c4fb234e970cd4070
Jenny Gebske <jenny@ctrl.alt.coop>
2021-01-25 11:00:56 +0100

    remove unused code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Events from '../views/Events.vue'
