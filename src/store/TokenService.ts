cbfca1300c41e7fb0239e916b205d94c1e2add1c
Jenny Gebske <jenny@ctrl.alt.coop>
2021-02-19 17:02:16 +0100

    add function to interact with localstorage

---
+++ src/store/TokenService.ts
@@ -0,0 +1,38 @@
+import { userStore } from '@/store/UserStore'
+
+const tokenService = {
+  getToken(tokenType: string) {
+    return localStorage.getItem(tokenType)
+  },
+
+  setToken(tokenType: string, token: string) {
+    userStore.setToken(token)
+    localStorage.setItem(tokenType, token)
+  },
+
+  removeToken(tokenType: string) {
+    userStore.setToken('')
+    localStorage.removeItem(tokenType)
+  },
+
+  getExpiry() {
+    return localStorage.getItem('expiry')
+  },
+
+  setExpiry(seconds: number) {
+    localStorage.setItem('expiry', JSON.stringify(new Date().getTime() + seconds))  
+  },
+
+  isTokenExpired() {
+    const expirationDate = this.getExpiry()
+    if (!expirationDate) return false
+    return parseInt(expirationDate) < new Date().getTime()
+  },
+
+  removeExpiry() {
+    localStorage.removeItem('expiry')
+  },
+
+}
+
+export { tokenService }
