59810cc95edec46c3bf7cbd6874781434d975953
Jenny Gebske <jenny@ctrl.alt.coop>
2021-02-23 12:48:47 +0100

    use axios: replace fetch

--- src/api/ApiRoute.ts
+++ src/api/ApiRoute.ts
@@ -1,6 +1,7 @@
 import { APIEnvelope } from '@/api/model/APIEnvelope'
 import { JSONResponse } from '@/api/JSONResponse'
 import { userStore } from '@/store/UserStore'
+import axios from 'axios'
 
 /**
  * Generic CRUD operation definitions for a route
@@ -23,41 +24,41 @@ export class ApiRoute<T, E = APIEnvelope<T>, L = APIEnvelope<T[]>> {
   async list(query: { [key: string]: any } = {}): Promise<JSONResponse<L>> {
     const url = new URL(`${this.baseUrl}/${this.path}`)
     Object.keys(query).forEach(key => url.searchParams.append(key, query[key]))
-    const response = await fetch(url.toString(), { headers: this.getHeader() })
-    const data = await response.json()
+    const response = await axios(url.toString(), { headers: this.getHeader() })
+    const data = await response.data
     return new JSONResponse<L>(response, data)
   }
 
   async get(id: string): Promise<JSONResponse<E>> {
-    const response = await fetch(`${this.baseUrl}/${this.path}/${id}`, {
+    const response = await axios(`${this.baseUrl}/${this.path}/${id}`, {
       headers: this.getHeader()
     })
-    const data = await response.json()
+    const data = await response.data
     return new JSONResponse<E>(response, data)
   }
 
   async create(body: Partial<T>): Promise<JSONResponse<E>> {
-    const response = await fetch(`${this.baseUrl}/${this.path}`, {
+    const response = await axios(`${this.baseUrl}/${this.path}`, {
       method: 'POST',
-      body: JSON.stringify(body),
+      data: JSON.stringify(body),
       headers: this.getHeader()
     })
-    const data = await response.json()
+    const data = await response.data
     return new JSONResponse<E>(response, data)
   }
 
   async update(id: string, body: T): Promise<JSONResponse<E>> {
-    const response = await fetch(`${this.baseUrl}/${this.path}/${id}`, {
+    const response = await axios(`${this.baseUrl}/${this.path}/${id}`, {
       method: 'PUT',
-      body: JSON.stringify(body),
+      data: JSON.stringify(body),
       headers: this.getHeader()
     })
-    const data = await response.json()
+    const data = await response.data
     return new JSONResponse<E>(response, data)
   }
 
   async delete(id: string): Promise<void> {
-    await fetch(`${this.baseUrl}/${this.path}/${id}`, {
+    await axios(`${this.baseUrl}/${this.path}/${id}`, {
       method: 'DELETE',
       headers: this.getHeader()
     })
--- src/api/EventRoute.ts
+++ src/api/EventRoute.ts
@@ -3,6 +3,7 @@ import { APIEnvelope } from '@/api/model/APIEnvelope'
 import { ApiRoute } from '@/api/ApiRoute'
 import { JSONResponse } from '@/api/JSONResponse'
 import { userStore } from '@/store/UserStore'
+import axios from 'axios'
 
 /**
  * A class extending {@link ApiRoute} to implement some extra non-standard operations (join / leave)
@@ -13,14 +14,14 @@ export class EventRoute extends ApiRoute<EventDto> {
    * @param id the event id
    */
   async join(id: string) {
-    const response = await fetch(`${this.baseUrl}/${this.path}/${id}/join`, {
+    const response = await axios(`${this.baseUrl}/${this.path}/${id}/join`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${userStore.getToken()}`,
         'Content-Type': 'application/json',
       }
     })
-    const data = await response.json()
+    const data = await response.data
     return new JSONResponse<APIEnvelope<EventDto>>(response, data)
   }
 
@@ -29,14 +30,14 @@ export class EventRoute extends ApiRoute<EventDto> {
    * @param id the event id
    */
   async leave(id: string) {
-    const response = await fetch(`${this.baseUrl}/${this.path}/${id}/leave`, {
+    const response = await axios(`${this.baseUrl}/${this.path}/${id}/leave`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${userStore.getToken()}`,
         'Content-Type': 'application/json'
       }
     })
-    const data = await response.json()
+    const data = await response.data
     return new JSONResponse<APIEnvelope<EventDto>>(response, data)
   }
 }
