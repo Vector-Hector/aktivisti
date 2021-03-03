b72e7b5c47a19b5c03a263f31ce6ba04329fc148
Jenny Gebske <jenny@ctrl.alt.coop>
2021-03-03 14:16:20 +0100

    add logout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,10 @@
       <IonLabel position="floating">
         Passwort
       </IonLabel>
-      <IonInput v-model="password" type="password" />
+      <IonInput
+        v-model="password"
+        type="password"
+      />
     </IonItem>
 
     <IonItem lines="none">
@@ -54,8 +57,6 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IonInput, IonLabel, IonItem, IonButton, IonCheckbox, IonItemDivider } from '@ionic/vue'
-import { userStore } from '@/store/UserStore'
-import { uiStore } from '@/store/UiStore'
 import { authService } from '@/api/authService'
 import { RouteLocation } from 'vue-router'
 
@@ -84,7 +85,6 @@ export default defineComponent({
   },
   methods: {
     async login() {
-
       const userParams = {
         grant_type: 'password',
         username: this.username,
