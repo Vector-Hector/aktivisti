a7a86a75b3b24872eee85e17d9ead0c51b428c9d
Jenny Gebske <jenny@ctrl.alt.coop>
2021-03-05 17:27:57 +0100

    persist location

--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -7,6 +7,8 @@ interface UserState {
   location: LocationDto | null
 }
 
+const LOCATION = 'LOCATION'
+
 class UserStore extends Store<UserState> {
   protected data(): UserState {
     return {
@@ -17,10 +19,25 @@ class UserStore extends Store<UserState> {
 
   public locate(location: LocationDto) {
     this.state.location = location
+    localStorage.setItem(LOCATION, JSON.stringify(location))
+  }
+
+  public getLocation() {
+    const locationString = localStorage.getItem(LOCATION)
+    let locationObject = null
+    if (locationString !== null) {
+      try {
+        locationObject = JSON.parse(locationString)
+      } catch (error) {
+        locationObject = null
+      }
+    }
+    return locationObject
   }
 
   public clearUser() {
     this.state.user = null
+    localStorage.removeItem(LOCATION)
   }
 
   public setUser(user: UserDto) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -70,7 +70,7 @@ export default defineComponent({
     AutoComplete
   },
   beforeRouteEnter(to, from, next) {
-    if (userStore.getState().location == null) {
+    if (userStore.getState().location == null && userStore.getLocation() == null) {
       next({ name: 'splash' })
     }
     next()
@@ -80,7 +80,7 @@ export default defineComponent({
       zoom: 14,
       iconWidth: 25,
       iconHeight: 40,
-      center: { ...userStore.getState().location },
+      center: { ...userStore.getState().location || userStore.getLocation() },
       events: [] as EventDto[],
       filteredCampaigns: [] as CampaignDto[],
       campaigns: [] as CampaignDto[],
