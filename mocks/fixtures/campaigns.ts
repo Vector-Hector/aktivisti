e34478dc04ae5ac1051704f8542e7f455bc23bfa
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-03-10 10:23:30 +0100

    fix fixtures to meet new dto spec

--- mocks/fixtures/campaigns.ts
+++ mocks/fixtures/campaigns.ts
@@ -5,13 +5,11 @@ export const sampleCampaigns: CampaignDto[] = [{
   name: 'Bundestagswahl 2021',
   start_date: '2021-05-07T00:00+01:00',
   end_date: '2021-09-26T23:59+01:00',
-  type: 1,
-  organization: 1,
+  campaign_type: 1
 }, {
   id: 2,
   name: 'Landtagswahl BaWü 2021',
   start_date: '2021-01-15T00:00+01:00',
   end_date: '2021-03-14T23:59+01:00',
-  type: 2,
-  organization: 2,
+  campaign_type: 2
 }]
