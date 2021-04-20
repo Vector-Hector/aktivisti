babd6fc2113d61078d2355497a942ab5c03b6d75
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-04-20 16:52:17 +0200

    Add social sharing buttons to the event

---
+++ src/utils/shareLinks.ts
@@ -0,0 +1,35 @@
+import { appendAsQueryParams } from '@/utils/url'
+import { FACEBOOK_SHARE_URL, MAIL_SHARE_URL, TWITTER_SHARE_URL, WHATSAPP_SHARE_URL } from '@/constants'
+
+
+export function createTwitterShareUrl(text: string, linkedUrl: string, tags: string[]): string {
+  const url = new URL(TWITTER_SHARE_URL)
+  appendAsQueryParams(url, {
+    text,
+    url: linkedUrl,
+    tags
+  })
+  return url.toString()
+}
+
+export function createFacebookShareUrl(linkedUrl: string): string {
+  const url = new URL(FACEBOOK_SHARE_URL)
+  appendAsQueryParams(url, {
+    u: linkedUrl,
+  })
+  return url.toString()
+}
+
+export function createWhatsappShareUrl(linkedUrl: string): string {
+  const url = new URL(WHATSAPP_SHARE_URL)
+  appendAsQueryParams(url, {
+    text: linkedUrl,
+  })
+  return url.toString()
+}
+
+
+export function createMailShareUrl(linkedUrl: string): string {
+  const subjectLine = 'Mach\' mit bei der Aktion von DIE LINKE'
+  return `${MAIL_SHARE_URL}?subject=${subjectLine}&body=${linkedUrl}`
+}
