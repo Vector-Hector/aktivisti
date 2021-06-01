388539194a8db5b30837a24193fca0d98d6d9088
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-06-01 14:59:31 +0200

    Use Boundingbox instead of center to determine edit map view

--- src/views/edit-event/EditEventMap.vue
+++ src/views/edit-event/EditEventMap.vue
@@ -1,9 +1,7 @@
 <template>
   <Map
     ref="map"
-    :center="initialCenter"
-    :zoom="initialZoom"
-    :boundingBox="bbox"
+    :bounding-box="bbox"
   >
     <router-view
       v-model:event="localEvent"
@@ -16,9 +14,8 @@
 import { defineComponent } from 'vue'
 import Map from '@/lib/mapbox/Map.vue'
 import EditEventMixin from '@/views/edit-event/EditEventMixin'
-import { booleanPointInPolygon, polygon, point, buffer, bbox } from '@turf/turf'
-import mapboxgl from 'mapbox-gl'
-
+import { point, buffer, bbox } from '@turf/turf'
+import { userStore } from '@/store/UserStore'
 
 export default defineComponent({
   name: 'EditEventMap',
@@ -28,38 +25,9 @@ export default defineComponent({
   mixins: [EditEventMixin],
   data() {
     return {
-      initialCenter: this.event.location ?? {
-        lat: 51.1642292,
-        lng: 10.4541194
-      },
-      initialZoom: this.event.location ? 14 : 11,
-      bbox: this.event.location ? bbox(buffer(point([this.event.location?.lng, this.event.location?.lat]), 3, {units:'kilometers'})) : undefined
-    }
-  },
-  watch: {
-    'event.location': {
-      handler(newValue) {
-        // check if the new location is still inside the bounds of the visual map - if not we should jump to it
-
-        // @ts-ignore
-        const map = this.$refs.map.map as mapboxgl.Map
-        // if not initialized return
-        if (!map || !newValue) return
-        const bounds = map.getBounds()
-        const boundsGeometry = polygon([
-          [
-            [bounds.getNorthWest().lng, bounds.getNorthWest().lat],
-            [bounds.getNorthEast().lng, bounds.getNorthEast().lat],
-            [bounds.getSouthEast().lng, bounds.getSouthEast().lat],
-            [bounds.getSouthWest().lng, bounds.getSouthWest().lat],
-            [bounds.getNorthWest().lng, bounds.getNorthWest().lat]
-          ]
-        ])
-        if (!booleanPointInPolygon([newValue.lng, newValue.lat], boundsGeometry)) {
-          this.initialCenter = newValue
-        }
-      },
-      deep: true
+      bbox:
+        this.event.location ? bbox(buffer(point([this.event.location?.lng, this.event.location?.lat]), 3, {units:'kilometers'}))
+          : (userStore.getState().bbox ?? undefined)
     }
   }
 })
