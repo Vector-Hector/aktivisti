a6c2da740c8b1af92d6d3c497702c5fe6e6d9360
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-06-06 12:56:25 +0200

    remove unused imports

--- src/components/PageLoadingSpinner.vue
+++ src/components/PageLoadingSpinner.vue
@@ -10,7 +10,7 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { QPage, QSpinnerPuff } from 'quasar'
+import { QSpinnerPuff } from 'quasar'
 
 export default defineComponent({
   name: 'PageLoadingSpinner',
