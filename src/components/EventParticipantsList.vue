f1f25384d97ade70fddc41a0342d6c528abe60e1
Lyudmila Vaseva <lyudmila@ctrl.alt.coop>
2021-06-17 13:55:33 +0200

    Add participants modal containing accepted invitees

---
+++ src/components/EventParticipantsList.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <QList v-if="participations.length > 0">
+        <QItem
+          v-for="participation in displayedParticipations"
+          :key="participation.id"
+        >
+          <QItemSection>
+            <QItemLabel v-if="participation.user_is_member">
+              <b>{{ participation.user_username }}</b> {{ participation.user_email }}
+            </QItemLabel>
+            <QItemLabel v-else>
+              {{ participation.user_email }}
+            </QItemLabel>
+          </QItemSection>
+
+          <QItemSection side>
+            <div
+              class="invitation-item-actions"
+            >
+              <QIcon
+                fill="none"
+                @click="verifyParticipation(participation.id)"
+              >
+                <QIcon
+                  :name="ionCheckmark"
+                  aria-label="Nutzer bestättigen"
+                />
+              </QIcon>
+              <QIcon
+                fill="none"
+                @click="deleteParticipation(participation.id)"
+              >
+                <QIcon
+                  :name="ionClose"
+                  aria-label="Nutzer von der Aktion entfernen"
+                />
+              </QIcon>
+            </div>
+          </QItemSection>
+
+        </QItem>
+        <QItem
+          v-if="pendingUsersWithoutVisibleEmailAddresses > 0"
+        >
+          <QItemSection>
+            <QItemLabel>
+              {{ pendingUsersWithoutVisibleEmailAddresses }} weitere per Mail eingeladen
+            </QItemLabel>
+          </QItemSection>
+        </QItem>
+      </QList>
+      <p v-else>
+        Keine Teilnehmer*innen. Nutze den "Leute einladen"-Button um welche einzuladen.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { EventParticipationDto } from 'src/api/model/EventParticipationDto'
+import { userStore } from 'src/store/UserStore'
+import { ionClose, ionCheckmark } from '@quasar/extras/ionicons-v5'
+import { QIcon, QItem, QItemLabel, QItemSection, QList } from 'quasar'
+
+
+export default defineComponent({
+  name: 'EventParticipantsList',
+  components: {
+    QList,
+    QItem,
+    QItemLabel,
+    QItemSection,
+    QIcon
+  },
+  props: {
+    eventId: {
+      type: Number as PropType<number>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      participations: [] as EventParticipationDto[],
+      ionClose,
+      ionCheckmark
+    }
+  },
+  computed: {
+    displayedParticipations(): EventParticipationDto[] {
+      return this.participations.filter((item) => !item.is_pending_invitation)
+    }
+  },
+  async created() {
+    this.participations = (await this.$apiClient.eventParticipations.list({event: this.eventId, is_verified: true})).payload.data
+  },
+  methods: {
+    async deleteParticipation(deleteId: number) {
+      this.participations = this.participations.filter(({id}) => deleteId !== id)
+      await this.$apiClient.eventParticipations.delete(deleteId.toString())
+    },
+
+    async verifyParticipation(participationId: number) {
+      await this.$apiClient.eventParticipations.verifyParticipant(participationId.toString())
+    }
+
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/modals/EventParticipantsModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <QDialog ref="dialog" @hide="onDialogHide">
+    <QCard class="participants-modal">
+      <QToolbar>
+        <QToolbarTitle>Mitmachende</QToolbarTitle>
+      </QToolbar>
+      <QCardSection>
+        <EventParticipantsList :event-id="eventId" />
+      </QCardSection>
+    </QCard>
+  </QDialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { QCard, QCardSection, QDialog, QToolbar, QToolbarTitle } from 'quasar'
+import EventParticipantsList from 'src/components/EventParticipantsList.vue'
+
+export default defineComponent({
+  name: 'EventParticipantsModal',
+  components: {
+    EventParticipantsList,
+    QDialog,
+    QCard,
+    QCardSection,
+    QToolbar,
+    QToolbarTitle,
+  },
+  emits: [
+    // REQUIRED
+    'ok', 'hide'
+  ],
+  props: {
+    eventId: {
+      type: Number as PropType<number>,
+      required: true
+    }
+  },
+  methods: {
+    show() {
+      // @ts-ignore
+      this.$refs.dialog.show()
+    },
+    hide() {
+      // @ts-ignore
+      this.$refs.dialog.hide()
+    },
+    onDialogHide() {
+      this.$emit('hide')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.participants-modal {
+  min-width: 320px;
+}
+</style>
