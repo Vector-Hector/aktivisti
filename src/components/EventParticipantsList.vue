2003a8113453e1824e7f6863f5db16697ec2f421
Peter <peter@ctrl.alt.coop>
2021-07-16 19:19:47 +0200

    CAC_FRONTEND-168 Add button to elevate user to team captain

--- src/components/EventParticipantsList.vue
+++ src/components/EventParticipantsList.vue
@@ -1,49 +1,17 @@
 <template>
   <div class="row">
     <div class="col">
-      <QList v-if="verifiedParticipations.length > 0">
-        <QItem
-          v-for="participation in verifiedParticipations"
-          :key="participation.id"
-        >
-          <QItemSection>
-            <QItemLabel v-if="participation.user_is_member">
-              <b>{{ participation.user_username }}</b> {{ participation.user_email }}
-            </QItemLabel>
-            <QItemLabel v-else>
-              {{ participation.user_email }}
-            </QItemLabel>
-          </QItemSection>
-
-          <QItemSection side>
-            <div
-              class="invitation-item-actions"
-            >
-              <QBtn
-                fill="none"
-                size="md"
-                :icon="ionClose"
-                dense
-                flat
-                round
-                @click="deleteParticipation(participation.id)"
-
-                aria-label="Nutzer von der Aktion entfernen"
-              />
-            </div>
-          </QItemSection>
-
-        </QItem>
-      </QList>
       <QList v-if="notVerifiedParticipations.length > 0">
         <QToolbarTitle>Teilnehmer*innen bestätigen</QToolbarTitle>
+        <QSeparator spced/>
         <QItem
           v-for="participation in notVerifiedParticipations"
           :key="participation.id"
         >
           <QItemSection>
             <QItemLabel v-if="participation.user_is_member">
-              <b>{{ participation.user_username }}</b> {{ participation.user_email }}
+              <q-item-label lines="1"><b>{{ participation.user_username}} </b></q-item-label>
+              <q-item-label caption>{{ participation.user_email }}</q-item-label>
             </QItemLabel>
             <QItemLabel v-else>
               {{ participation.user_email }}
@@ -80,6 +48,87 @@
 
         </QItem>
       </QList>
+      <QList v-if="verifiedParticipations.length > 0">
+        <QToolbarTitle>Bestätigte Teilnehmer*innen</QToolbarTitle>
+        <QSeparator spced/>
+        <QItem
+          v-for="participation in verifiedParticipations"
+          :key="participation.id"
+        >
+          <QItemSection>
+            <QItemLabel v-if="participation.user_is_member">
+              <q-item-label lines="1"><b>{{ participation.user_username}} </b></q-item-label>
+              <q-item-label caption>{{ participation.user_email }}</q-item-label>
+            </QItemLabel>
+            <QItemLabel v-else>
+              {{ participation.user_email }}
+            </QItemLabel>
+          </QItemSection>
+
+          <QItemSection side>
+            <div
+              class="invitation-item-actions"
+            >
+              <QBtn
+                fill="none"
+                size="md"
+                :icon="ionClose"
+                dense
+                flat
+                round
+                @click="deleteParticipation(participation.id)"
+                aria-label="Nutzer von der Aktion entfernen"
+              />
+              <QBtn
+                fill="none"
+                size="md"
+                :icon="matArrowCircleUp"
+                dense
+                flat
+                round
+                @click="handleInviteToTeamCaptain(participation.user, participation.user_username)"
+                aria-label="Nutzer zu einem Teamcaptain machen"
+              />
+            </div>
+          </QItemSection>
+
+        </QItem>
+      </QList>
+      <QList v-if="areTeamCaptainsParticipations.length > 0">
+        <QToolbarTitle>Team Captains</QToolbarTitle>
+        <QSeparator spced/>
+        <QItem
+          v-for="participation in areTeamCaptainsParticipations"
+          :key="participation.id"
+        >
+          <QItemSection>
+            <QItemLabel v-if="participation.user_is_member">
+              <q-item-label lines="1"><b>{{ participation.user_username}} </b></q-item-label>
+              <q-item-label caption>{{ participation.user_email }}</q-item-label>
+            </QItemLabel>
+            <QItemLabel v-else>
+              {{ participation.user_email }}
+            </QItemLabel>
+          </QItemSection>
+          <QItemSection side>
+            <div
+              class="invitation-item-actions"
+            >
+              <QBtn
+                fill="none"
+                size="md"
+                :icon="ionClose"
+                dense
+                flat
+                round
+                @click="deleteParticipation(participation.id)"
+
+                aria-label="Nutzer von der Aktion entfernen"
+              />
+            </div>
+          </QItemSection>
+        </QItem>
+      </QList>
       <p v-if="verifiedParticipations.length === 0 && notVerifiedParticipations.length === 0">
         Keine Teilnehmer*innen. Nutze den "Leute einladen"-Button um welche einzuladen.
       </p>
@@ -90,8 +139,17 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { EventParticipationDto } from 'src/api/model/EventParticipationDto'
-import { ionClose, ionCheckmark } from '@quasar/extras/ionicons-v5'
-import { QBtn, QItem, QItemLabel, QItemSection, QList, QToolbarTitle } from 'quasar'
+import { ionClose, ionCheckmark, ionTrash } from '@quasar/extras/ionicons-v5'
+import { matArrowCircleUp } from '@quasar/extras/material-icons'
+import { QBtn, QItem, QItemLabel, QItemSection, QList, QSeparator, QToolbarTitle } from 'quasar'
+import { apiClient } from 'src/api/ApiClient';
+import { userStore } from 'src/store/UserStore';
+import { ObjectPermissions } from 'src/api/model/ObjectPermissionDto';
+import { ContentType } from 'src/api/model/UserObjectPermissionDto';
+
+interface EventParticipationNTeamCaptain extends EventParticipationDto {
+  is_team_captain?: boolean
+}
 
 export default defineComponent({
   name: 'EventParticipantsList',
@@ -101,7 +159,8 @@ export default defineComponent({
     QItemLabel,
     QItemSection,
     QBtn,
-    QToolbarTitle
+    QToolbarTitle,
+    QSeparator,
   },
   props: {
     eventId: {
@@ -115,26 +174,43 @@ export default defineComponent({
   },
   data() {
     return {
-      participations: [] as EventParticipationDto[],
+      participations: [] as EventParticipationNTeamCaptain[],
       ionClose,
-      ionCheckmark
+      ionCheckmark,
+      ionTrash,
+      matArrowCircleUp
     }
   },
   computed: {
-    verifiedParticipations(): EventParticipationDto[] {
-      return this.participations.filter((item) => item.is_verified)
+    verifiedParticipations(): EventParticipationNTeamCaptain[] {
+      return this.participations.filter(({is_verified, is_team_captain}) => !is_team_captain && is_verified)
     },
-    notVerifiedParticipations(): EventParticipationDto[] {
-      return this.participations.filter((item) => !item.is_verified)
+    notVerifiedParticipations(): EventParticipationNTeamCaptain[] {
+      return this.participations.filter(({is_verified, is_team_captain}) => !is_team_captain && !is_verified)
+    },
+    areTeamCaptainsParticipations(): EventParticipationNTeamCaptain[] {
+      return this.participations.filter(({is_team_captain}) => is_team_captain)
     }
   },
   async created() {
+    const responseUserPermissions = await apiClient.userPermissions.list()
+    const permissions = responseUserPermissions.payload.data
+    userStore.setPermissions(permissions)
     this.participations = (await
       this.$apiClient.eventParticipations.list(
         {
           event: this.eventId,
           is_pending_invitation: false
-        })).payload.data
+        })).payload.data as EventParticipationNTeamCaptain[]
+    this.participations.map((participation) => {
+      const isTeamcaptain = permissions.some(({user, permission_codename, content_type_name, object_pk}) => {
+        return participation.user === user &&
+          permission_codename === ObjectPermissions.TeamCaptain &&
+          content_type_name === ContentType.SUB_ASSOCIATION &&
+          object_pk === this.eventSubAssociation?.toString()
+      })
+      participation['is_team_captain'] = isTeamcaptain
+    })
   },
   methods: {
     async deleteParticipation(deleteId: number) {
@@ -148,6 +224,19 @@ export default defineComponent({
         is_verified: true
       })
       this.participations[participationIndex] = participationRequest.payload.data
+    },
+    async elevateToTeamCaptain(userId: number) {
+      await apiClient.user.elevateToTeamCaptain(userId.toString(), this.eventSubAssociation)
+      const participation = this.participations.find(({user}) => user === userId)
+      participation!['is_team_captain'] = true
+    },
+    handleInviteToTeamCaptain(userId: number, username: string) {
+      this.$q.dialog({
+        title: 'Benutzer zu einem Teamcaptain hochstufen',
+        message: `Möchtest du den Nutzer <b>${username}</b> zu einem Teamcaptain hochstufen?`,
+        html: true,
+        cancel: true
+      }).onOk(() => this.elevateToTeamCaptain(userId))
     }
 
   }
