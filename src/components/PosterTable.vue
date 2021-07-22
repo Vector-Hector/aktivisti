e533351e5bc64c13e8f416e5c57da1020c670bf1
Jonatan Zint <jonatan@ctrl.alt.coop>
2021-07-22 14:53:57 +0200

    Move Poster table into reusable component CAC_FRONTEND-225

---
+++ src/components/PosterTable.vue
@@ -0,0 +1,143 @@
+<template>
+  <QTable
+    :auto-layout="true"
+    @row-click="(event, row, _) => $emit('rowClick', row)"
+    flat
+    dense
+    :columns="posterColumns"
+    :rows="posters"
+    row-key="name"
+    virtual-scroll
+    hide-pagination
+    :rows-per-page-options="[0]"
+    class="editable-cells-table overflow-hidden q-my-sm poster-table"
+    edit-mode="cell"
+    no-data-label="Noch keine Poster erstellt"
+
+  >
+    <template v-slot:header="props">
+      <QTr :props="props">
+        <QTh
+          v-for="col in props.cols"
+          :key="col.name"
+          :props="props"
+          class="table-header"
+        >
+          {{ col.label }}
+        </QTh>
+      </QTr>
+    </template>
+    <template v-slot:body-cell-status="props">
+      <QTd key="status" :props="props">
+        <StatusRow :status="props.row.status" />
+      </QTd>
+    </template>
+    <template v-slot:body-cell-actions="props">
+      <QTd key="actions" :props="props">
+        <QBtn
+          dense
+          round
+          flat
+          color="grey-6"
+          :icon="ionTrash"
+          @click.prevent.stop="onDeleteClicked(props.row)"
+        />
+      </QTd>
+    </template>
+  </QTable>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ellipsis } from 'src/utils/string'
+import { PosterDto } from 'src/api/model/PosterDto'
+import StatusRow from 'components/StatusRow.vue'
+import { QBtn, QTable, QTd, QTh, QTr } from 'quasar'
+import { ionTrash } from '@quasar/extras/ionicons-v5'
+
+export default defineComponent({
+  name: 'PosterTable',
+  components: {
+    StatusRow,
+    QTable,
+    QBtn,
+    QTr,
+    QTh,
+    QTd
+  },
+  props: {
+    posters: {
+      type: Array as PropType<PosterDto>,
+      required: true
+    },
+    showActions: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
+  },
+  emits: ['rowClick'],
+  computed: {
+    posterColumns() {
+      return [{
+        name: 'poster_id',
+        label: '#',
+        field: 'poster_id',
+        align: 'right',
+        required: true
+      }, {
+        name: 'location_description',
+        label: 'Ort',
+        field: 'location_description',
+        align: 'left',
+        required: true,
+        format: (value: string) => value ? ellipsis(value, 25) : 'Unbenannter Ort'
+      }, {
+        name: 'status',
+        label: 'Status',
+        field: 'status',
+        align: 'left'
+      }, {
+        name: 'actions',
+        label: '',
+        field: null,
+        required: true
+      }]
+    }
+  },
+  data() {
+    return {
+      ionTrash
+
+    }
+  },
+  methods: {
+    async onDeleteClicked(poster: PosterDto) {
+      if (this.confirmDelete) {
+        this.$q.dialog({
+          title: 'Poster löschen',
+          message: `Möchtest du Poster #${poster.poster_id} wirklich löschen?`,
+          options: {
+            type: 'checkbox',
+            model: [],
+            items: [
+              {label: 'Beim nächsten mal nicht mehr fragen', value: 'skipConfirm'}
+            ]
+          },
+          cancel: true
+        }).onOk((data: any) => {
+          if (data.includes('skipConfirm')) {
+            this.confirmDelete = false
+          }
+          void this.deletePoster(poster)
+        })
+      } else {
+        await this.deletePoster(poster)
+      }
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.table-header {
+  background: $grey-3;
+}
+</style>
--- src/pages/edit-event/posters/edit-list/EditEventPostersList.vue
+++ src/pages/edit-event/posters/edit-list/EditEventPostersList.vue
@@ -10,52 +10,11 @@
         />
       </div>
     </div>
-    <QTable
-      :auto-layout="true"
-      @row-click="(event, row, _) => editPoster(row.id)"
-      flat
-      dense
-      :columns="posterColumns"
-      :rows="posters"
-      row-key="name"
-      virtual-scroll
-      hide-pagination
-      :rows-per-page-options="[0]"
-      class="editable-cells-table overflow-hidden q-my-sm poster-table"
-      edit-mode="cell"
-      no-data-label="Noch keine Poster erstellt"
-
-    >
-      <template v-slot:header="props">
-        <QTr :props="props">
-          <QTh
-            v-for="col in props.cols"
-            :key="col.name"
-            :props="props"
-            class="table-header"
-          >
-            {{ col.label }}
-          </QTh>
-        </QTr>
-      </template>
-      <template v-slot:body-cell-status="props">
-        <QTd key="status" :props="props">
-          <StatusRow :status="props.row.status" />
-        </QTd>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <QTd key="actions" :props="props">
-          <QBtn
-            dense
-            round
-            flat
-            color="grey-6"
-            :icon="ionTrash"
-            @click.prevent.stop="onDeleteClicked(props.row)"
-          />
-        </QTd>
-      </template>
-    </QTable>
+    <PosterTable
+      :show-actions="true"
+      :posters="posters"
+      @row-click="editPoster($event.id)"
+    />
   </div>
   <SidebarBottomNavigation
     @close="abort"
@@ -69,31 +28,23 @@
 import { defineComponent, inject } from 'vue'
 
 import {
-  QBtn,
-  QTable,
-  QTd, QTh,
-  QTr
+  QBtn
 } from 'quasar'
-import { ionLocationSharp, ionPencil, ionTrash } from '@quasar/extras/ionicons-v5'
+import { ionLocationSharp } from '@quasar/extras/ionicons-v5'
 import EditEventGeometryMixin from 'pages/edit-event/geometry/EditEventGeometryMixin'
 import SidebarBottomNavigation from 'components/SidebarBottomNavigation.vue'
 import EditEventAutoSaveMixin from 'pages/edit-event/EditEventAutoSaveMixin'
-import { PosterDto, posterStatusOptions } from 'src/api/model/PosterDto'
+import { posterStatusOptions } from 'src/api/model/PosterDto'
 import { StepControls } from 'pages/EditEvent.vue'
 import EditPosterListMixin from 'pages/edit-event/posters/EditPosterListMixin'
-import { ellipsis } from 'src/utils/string'
-import StatusRow from 'components/StatusRow.vue'
+import PosterTable from 'components/PosterTable.vue'
 
 export default defineComponent({
   name: 'EditEventPostersList',
   components: {
-    StatusRow,
+    PosterTable,
     SidebarBottomNavigation,
-    QBtn,
-    QTable,
-    QTd,
-    QTr,
-    QTh
+    QBtn
   },
   mixins: [EditEventGeometryMixin, EditPosterListMixin, EditEventAutoSaveMixin],
   setup() {
@@ -103,38 +54,12 @@ export default defineComponent({
   },
   data() {
     return {
-      ionPencil,
-      ionTrash,
       ionLocationSharp,
       posterStatusOptions,
       confirmDelete: true,
       filters: {
         status: null
-      },
-      posterColumns: [{
-        name: 'poster_id',
-        label: '#',
-        field: 'poster_id',
-        align: 'right',
-        required: true
-      }, {
-        name: 'location_description',
-        label: 'Ort',
-        field: 'location_description',
-        align: 'left',
-        required: true,
-        format: (value: string) => value ? ellipsis(value, 25) : 'Unbenannter Ort'
-      }, {
-        name: 'status',
-        label: 'Status',
-        field: 'status',
-        align: 'left'
-      }, {
-        name: 'actions',
-        label: '',
-        field: null,
-        required: true
-      }]
+      }
     }
   },
   methods: {
@@ -149,29 +74,6 @@ export default defineComponent({
     async abort() {
       await this.saveDebouncer.waitForSettle()
       this.stepControls.abort()
-    },
-    async onDeleteClicked(poster: PosterDto) {
-      if (this.confirmDelete) {
-        this.$q.dialog({
-          title: 'Poster löschen',
-          message: `Möchtest du Poster #${poster.poster_id} wirklich löschen?`,
-          options: {
-            type: 'checkbox',
-            model: [],
-            items: [
-              {label: 'Beim nächsten mal nicht mehr fragen', value: 'skipConfirm'}
-            ]
-          },
-          cancel: true
-        }).onOk((data: any) => {
-          if (data.includes('skipConfirm')) {
-            this.confirmDelete = false
-          }
-          void this.deletePoster(poster)
-        })
-      } else {
-        await this.deletePoster(poster)
-      }
     },
     async editPoster(posterId: number) {
       await this.$router.push({
@@ -200,10 +102,6 @@ export default defineComponent({
   font-size: 1.5rem;
   line-height: 2rem;
   font-weight: bold;
-}
-
-.table-header {
-  background: $grey-3;
 }
 
 .buttons {
