e5cbfa12f90ffa9d109ea58c3deba652c90db40c
Peter Strümpel <peter@ctrl.alt.coop>
2021-07-26 12:02:16 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/PosterTable.vue
+++ src/components/PosterTable.vue
@@ -93,7 +93,7 @@ export default defineComponent({
         name: 'status',
         label: 'Status',
         field: 'status',
-        align: 'left'
+        align: 'right'
       }]
       if (this.showActions) {
         columns.push({
