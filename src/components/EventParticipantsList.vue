20c02f22799b22f7d2b1899153dfb98936ead6c4
Jan Gerber <j@open-video.com>
2021-09-12 14:41:50 +0200

    don't show upgrade to team captain action for event coordinators

--- src/components/EventParticipantsList.vue
+++ src/components/EventParticipantsList.vue
@@ -68,6 +68,7 @@
                 aria-label="Nutzer:in von der Aktion entfernen"
               />
               <QBtn
+                v-if="!participation.is_event_coordinator"
                 fill="none"
                 size="md"
                 :icon="matArrowCircleUp"
