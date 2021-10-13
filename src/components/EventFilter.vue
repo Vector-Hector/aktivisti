40e4bfcb9393970ad4ae258ef083247efd2f23c8
Peter <peter@ctrl.alt.coop>
2021-10-13 13:08:09 +0200

    CAC_FRONTEND-279 Extract default filter input field
    There have been some duplication in the usage of QSelect
    for the event filters, to mitigate that a new component is created, which sets some values in beforehand.
    
    relevant-links:
    - https://gist.github.com/loilo/73c55ed04917ecf5d682ec70a2a1b8e2

--- src/components/EventFilter.vue
+++ src/components/EventFilter.vue
@@ -1,93 +1,73 @@
 <template>
-  <div class="select-wrapper">
-    <QSelect
-      :dropdownIcon="ionChevronDown"
-      :clearIcon="ionClose"
-      filled
-      label="Kampagnen"
-      :model-value="filterParams.campaigns?.[0] ?? 0"
-      @update:model-value="updateCampaign"
-      :options="campaignOptions"
-      emit-value
-      map-options
-      option-value="id"
-      option-label="name"
-    />
-  </div>
-  <div class="select-wrapper">
-    <QSelect
-      class="filter-dropdown"
-      label="Bezirks/Kreisverband"
-      :dropdownIcon="ionChevronDown"
-      :clearIcon="ionClose"
-      filled
-      multiple
-      :model-value="filterParams.sub_association"
-      @update:model-value="updateSubAssociations"
-      use-input
-      use-chips
-      emit-value
-      map-options
-      clearable
-      input-debounce="0"
-      :options="suggestedSubassociations"
-      @filter="filterSubAssociations"
-      option-value="id"
-      option-label="name"
-    >
-      <template v-slot:no-option>
-        <q-item>
-          <q-item-section class="text-grey">
-            Kein Verband gefunden
-          </q-item-section>
-        </q-item>
-      </template>
-    </QSelect>
-  </div>
-  <div class="select-wrapper">
-    <QSelect
-      class="filter-dropdown"
-      :dropdownIcon="ionChevronDown"
-      :clearIcon="ionClose"
-      filled
-      :model-value="filterParams.order_by"
-      @change="updateSorting"
-      input-debounce="0"
-      label="Sortierung"
-      :options="sortOptions"
-      :option-label="(item) => SortOptionLabels[item]"
-      placeholder="Sortierung auswählen"
-    />
-  </div>
-    <div class="select-wrapper">
-    <QSelect
-      class="filter-dropdown"
-      :dropdownIcon="ionChevronDown"
-      :clearIcon="ionClose"
-      filled
-      :model-value="filterParams.event_type"
-      @update:model-value="updateEventType"
-      input-debounce="0"
-      label="Aktionstyp"
-      :options="eventTypeOptions"
-      emit-value
-      map-options
-      option-value="key"
-      option-label="label"
-      clearable
+  <FilterInput
+    label="Kampagnen"
+    :model-value="filterParams.campaigns?.[0] ?? 0"
+    @update:model-value="updateCampaign"
+    :options="campaignOptions"
+    emit-value
+    map-options
+    option-value="id"
+    option-label="name"
+  />
+  <FilterInput
+    class="filter-dropdown"
+    label="Bezirks/Kreisverband"
+    multiple
+    :model-value="filterParams.sub_association"
+    @update:model-value="updateSubAssociations"
+    use-input
+    use-chips
+    emit-value
+    map-options
+    clearable
+    input-debounce="0"
+    :options="suggestedSubassociations"
+    @filter="filterSubAssociations"
+    option-value="id"
+    option-label="name"
+  >
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          Kein Verband gefunden
+        </q-item-section>
+      </q-item>
+    </template>
+  </FilterInput>
+  <FilterInput
+    class="filter-dropdown"
+    :model-value="filterParams.order_by"
+    @change="updateSorting"
+    input-debounce="0"
+    label="Sortierung"
+    :options="sortOptions"
+    :option-label="(item) => SortOptionLabels[item]"
+    placeholder="Sortierung auswählen"
+  />
+  <FilterInput
+    class="filter-dropdown"
+    :model-value="filterParams.event_type"
+    @update:model-value="updateEventType"
+    input-debounce="0"
+    label="Aktionstyp"
+    :options="eventTypeOptions"
+    emit-value
+    map-options
+    option-value="key"
+    option-label="label"
+    clearable
 
-    />
-  </div>
+  />
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { QSelect } from 'quasar'
 import { SortOption } from 'src/store/UserStore'
 import { CampaignDto } from 'src/api/model/CampaignDto'
 import { ionChevronDown, ionClose } from '@quasar/extras/ionicons-v5'
 import { SubAssociationDto } from 'src/api/model/SubAssociationDto'
 import { eventTypeOptions, EventTypes } from 'src/api/model/EventTypes'
 import { EventFilterParams } from 'src/api/params/EventFilterParams'
+import FilterInput from 'components/FilterInput.vue'
 
 const SortOptionLabels = {
   [SortOption.START_DATE]: 'Datum (Beginn)',
@@ -97,7 +77,7 @@ const SortOptionLabels = {
 export default defineComponent({
   name: 'EventFilter',
   components: {
-    QSelect
+    FilterInput
   },
   props: {
     filterParams: {
@@ -183,8 +163,3 @@ export default defineComponent({
   }
 })
 </script>
-<style lang="scss" scoped>
-.select-wrapper {
-  margin: 0 0 .5rem 0;
-}
-</style>
---
+++ src/components/FilterInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="select-wrapper">
+    <QSelect
+      :dropdownIcon="ionChevronDown"
+      :clearIcon="ionClose"
+      filled
+      :model-value="modelValue"
+      v-bind="$attrs">
+      <template v-for="(_, name) in $slots" v-slot:[name]="slotData">
+        <slot :name="name" v-bind="slotData" />
+      </template>
+    </QSelect>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ionChevronDown, ionClose } from '@quasar/extras/ionicons-v5'
+import { QSelect } from 'quasar'
+
+export default defineComponent({
+  name: 'FilterInput',
+  components: {
+    QSelect
+  },
+  props: {
+    modelValue: {
+      type: [Number, String, Array] as PropType<typeof QSelect.$props>
+      //  TODO(peter@ctrl.alt.coop) modelValue should be required, but passed modelValue is sometimes undefined
+      //  It needs to be checked which passed modelValues are undefined and to fix this.
+    }
+  },
+  data() {
+    return {
+      ionChevronDown,
+      ionClose
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.select-wrapper {
+  margin: 0 0 .5rem 0;
+}
+</style>
