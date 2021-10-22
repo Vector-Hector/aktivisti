d92a98f48b8146a18a3c8ef9b28dca3a4606a483
Peter <peter@ctrl.alt.coop>
2021-10-22 14:07:27 +0200

    CAC_FRONTEND-256 Add marker for selected map items

---
+++ src/components/SelectedMarker.vue
@@ -0,0 +1,37 @@
+<template>
+  <Marker
+    :location="location"
+  >
+    <template v-slot:marker>
+      <QIcon
+        name="img:static/icons/map-pin-selected.svg"
+        class="clickable-maker"
+        size="xl"
+      />
+    </template>
+  </Marker>
+
+</template>
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue'
+import Marker from 'src/mapbox/Marker.vue'
+import { QIcon } from 'quasar'
+import { LocationDto } from 'src/api/model/LocationDto'
+
+export default defineComponent({
+  name: 'SelectedMarker',
+  components: {Marker, QIcon},
+  props: {
+    location: {
+      type: Object as PropType<LocationDto>,
+      required: true
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.clickable-marker {
+  cursor: pointer;
+}
+</style>
