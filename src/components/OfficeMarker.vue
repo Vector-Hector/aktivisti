cfed19d5f1551c8defc46fbe8d093b32681cd736
Peter <peter@ctrl.alt.coop>
2021-10-22 14:07:43 +0200

    CAC_FRONTEND-256 Add marker for offices

---
+++ src/components/OfficeMarker.vue
@@ -0,0 +1,38 @@
+<template>
+  <Marker
+    :location="location"
+  >
+    <template v-slot:marker>
+      <QIcon
+        name="img:static/icons/map-pin-office.svg"
+        class="clickable-marker"
+        size="lg"
+      />
+    </template>
+    <slot />
+  </Marker>
+</template>
+
+<script lang="ts">
+
+
+import { defineComponent, PropType } from 'vue'
+import { LocationDto } from 'src/api/model/LocationDto'
+import Marker from 'src/mapbox/Marker.vue'
+import { QIcon } from 'quasar'
+
+export default defineComponent({
+  components: {Marker, QIcon},
+  props: {
+    location: {
+      type: Object as PropType<LocationDto>,
+      required: true
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.clickable-marker {
+  cursor: pointer;
+}
+</style>
