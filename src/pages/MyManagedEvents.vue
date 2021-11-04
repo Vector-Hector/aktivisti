007842c009bd0931a137b3e8e94a7b7a461afa1b
Peter <peter@ctrl.alt.coop>
2021-11-04 15:37:33 +0100

    CAC_FRONTEND-316 Exclude managed events into extra component

--- src/pages/MyManagedEvents.vue
+++ src/pages/MyManagedEvents.vue
@@ -1,25 +1,7 @@
 <template>
   <QPage class="flex-fill">
     <div class="container my-managed-events">
-      <div class="filter-content">
-        <EventFilter
-          v-model:filter-params="filterParams"
-          :is-ownership-filterable="true"
-          :is-campaign-filterable="false"
-          :is-sub-association-filterable="false"
-          :is-sort-order-configurable="false"
-          :is-event-type-filterable="true"
-        />
-      </div>
-      <EventList
-        v-model:events="shownEvents"
-        v-model:pagination="pagination"
-        :filter-params="filterParams"
-        :campaigns="campaigns"
-        class="event-list"
-        ref="eventList"
-        @clickOnEvent="goToEvent"
-      />
+      <ManagedEvents/>
     </div>
   </QPage>
 </template>
@@ -27,92 +9,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { QPage } from 'quasar'
-import { ionChevronDown, ionClose } from '@quasar/extras/ionicons-v5';
+import ManagedEvents from 'components/ManagedEvents.vue'
 
-import { CampaignDto } from 'src/api/model/CampaignDto';
-import EventList from 'components/EventList.vue';
-import { Pagination } from 'src/api/model/APIEnvelope';
-import { EVENT_LIST_CHUNK_SIZE } from 'src/constants'
-import { EventDto } from 'src/api/model/EventDto';
-import { EventStatus } from 'src/api/model/EventStatus'
-import EventFilter from 'components/EventFilter.vue'
-import { isEqual } from 'lodash-es'
-
-const _defaultPagination = {
-  limit: EVENT_LIST_CHUNK_SIZE
-}
 
 export default defineComponent({
   name: 'MyManagedEvents',
   components: {
-    EventFilter,
-    EventList,
+    ManagedEvents,
     QPage,
   },
-  async created() {
-    await this.updateShownEvents()
-    await this.updateCampaigns()
-  },
-  data() {
-    return {
-      campaigns: [] as CampaignDto[],
-      filterParams: {
-        is_owner: true,
-        management_permission: true,
-        status: EventStatus.ACTIVE
-      } as Record<string, number | string | boolean>,
-      ionChevronDown,
-      ionClose,
-      pagination: _defaultPagination as Pagination | null,
-      shownEvents: [] as EventDto[],
-    }
-  },
-  methods: {
-    goToEvent(event: EventDto) {
-      void this.$router.push({
-        name: 'event-detail',
-        params: {
-          eventId: event.id
-        }
-      })
-    },
-    resetPagination() {
-      this.pagination = _defaultPagination as Pagination
-    },
-    async updateCampaigns() {
-      this.campaigns = (await this.$apiClient.campaigns.list()).payload.data
-    },
-    async updateShownEvents() {
-      try {
-        const {data: events, pagination} = (await this.$apiClient.events.list(
-          {
-            ...this.pagination,
-            ...this.filterParams
-          }
-        )).payload
-        this.pagination = pagination!
-        this.shownEvents = events
-      } catch {
-        this.$q.notify({
-          message: 'Etwas ging schief beim Abrufen der Aktionen',
-          color: 'negative'
-        })
-      }
-    },
-  },
-  watch: {
-    filterParams: {
-      async handler(newValue, oldValue){
-        if(isEqual(newValue,oldValue)) return
-        // @ts-ignore
-        this.$refs.eventList?.resetScrollPosition()
-        this.resetPagination()
-        await this.updateShownEvents()
-      },
-      deep: true,
-      immediate: true
-    }
-  }
 })
 </script>
 
@@ -123,16 +28,6 @@ export default defineComponent({
   height: 100%;
   display: flex;
   flex-direction: column;
-}
-
-.event-list {
-  margin: 1rem 0;
-  height: 100%;
-  overflow: hidden;
-}
-
-.filter-content {
-  padding: 1rem;
 }
 </style>
 
