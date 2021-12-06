df0be79783f0bb2e40078e1a60b9c4cf91596cba
Peter <peter@ctrl.alt.coop>
2021-12-06 10:49:34 +0100

    CAC_FRONTEND-315 QA Hide posters in sidebar conditionally
    relevant-links:
    - https://gitlab.alt.coop/wahlkampf-app/wk-frontend/-/issues/315#note_27253

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -87,7 +87,9 @@
             <span class="menu-item-link-text">DIE LINKE vor Ort</span>
           </MenuLink>
         </div>
-        <div class="menu-item">
+        <div
+          v-if="hasManagePermission"
+          class="menu-item">
           <MenuLink
             to="/posters"
           >
@@ -138,7 +140,7 @@
           <MenuLink
             :to="{ name: 'manage-users'}"
           >
-            <QIcon :name="ionPeopleOutline"/>
+            <QIcon :name="ionPeopleOutline" />
             <span class="menu-item-link-text">Benutzer*innen verwalten</span>
           </MenuLink>
         </div>
@@ -295,6 +297,7 @@ body.platform-ios {
 ::v-deep(.navigation-sidebar) {
   position: relative;
   overflow: visible;
+
   .menu-button {
     visibility: visible !important;
     color: $grey-8;
