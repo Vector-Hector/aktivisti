65598aca5a3dbe0fafadb5be02dd169cea20491f
Peter <peter@ctrl.alt.coop>
2022-02-17 19:09:41 +0100

    CAC_FRONTEND-327 Add FlyerMetricChart

---
+++ src/components/reportCharts/eventMetrics/FlyerMetricChart.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import { onBeforeMount, ref } from 'vue'
+import EventMetricChart from 'components/reportCharts/eventMetrics/EventMetricChart.vue'
+import { EventMetricDto } from 'src/api/model/EventMetricDto'
+import { EventTypes } from 'src/api/model/EventTypes'
+import { apiClient } from 'src/api/ApiClient'
+
+interface Props {
+  campaignId: number,
+  stateAssociationId?: number,
+  subAssociationId?: number,
+}
+
+const props = defineProps<Props>()
+
+const metricsFlyer = ref<EventMetricDto[] | null>(null)
+
+onBeforeMount(async () => {
+  metricsFlyer.value = await fetchEventMetric(EventTypes.FLYERS)
+})
+
+async function fetchEventMetric(eventType: EventTypes) {
+  return (await apiClient.eventMetrics.list({available_for_types: eventType})).payload.data
+
+}
+</script>
+<template>
+  <EventMetricChart
+    :campaignId="props.campaignId"
+    :stateAssociationId="props.stateAssociationId"
+    :subAssociationId="props.subAssociationId"
+    :eventMetrics="metricsFlyer"
+    title="Flyer"
+  />
+</template>
