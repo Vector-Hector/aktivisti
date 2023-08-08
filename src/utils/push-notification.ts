ec66ede5f0a54b86401ffbfb7b31f58e8aaf0b3d
James Jenkinson <james@ctrl.alt.coop>
2023-08-08 10:52:33 +0200

    Fix linter

--- src/utils/push-notification.ts
+++ src/utils/push-notification.ts
@@ -55,7 +55,8 @@ async function registerWebDevice(): Promise<void> {
   if (existingSubscription != null)
     return registerSubscription(existingSubscription)
 
-  const publicVapidToken = (await apiClient.account.getVapidPublicKey()).payload.data.key
+  const publicVapidToken = (await apiClient.account.getVapidPublicKey()).payload
+    .data.key
   const convertedToken = urlBase64ToUint8Array(publicVapidToken)
   const newSubscription = await serviceWorkerRegistration.pushManager.subscribe(
     {
