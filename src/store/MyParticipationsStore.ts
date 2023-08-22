172452a2f148fa9c9af8c2bdd1ab8e30bbdfbada
Peter <peter@ctrl.alt.coop>
2023-08-22 14:37:33 +0200

    CAC_FRONTEND-410 Add management permission to interface

--- src/store/MyParticipationsStore.ts
+++ src/store/MyParticipationsStore.ts
@@ -1,7 +1,10 @@
 import { Store } from 'src/store/Store'
 import { EventFilterPreferences, SortOption } from 'src/store/UserStore'
 import { EventParticipationDto } from 'src/api/model/EventParticipationDto'
-
+//TODO(peter) Remove filter Preferences
+// It seems like the `fitlerPrerences` of `myParticipationsStore` aren't used,
+// so they could be removed, also with the depending function to set it.
+// It might also be good Refactor this store completly.
 interface MyParticipationsStoreState {
   filterPreferences: EventFilterPreferences
   eventParticipations: EventParticipationDto[]
@@ -15,7 +18,9 @@ class MyParticipationsStore extends Store<MyParticipationsStoreState> {
         campaign: undefined,
         sorting: SortOption.START_DATE,
         eventType: undefined,
-        status: undefined
+        status: undefined,
+        is_owner: undefined,
+        management_permission: undefined
       },
       eventParticipations: [] as EventParticipationDto[]
     }
