bb10eb253d5fbd73e5a67e1aded799b053f4f9b8
Peter <peter@ctrl.alt.coop>
2023-08-22 14:46:04 +0200

    CAC_FRONTEND-410 Create Filter to only show editable events

---
+++ src/components/filterInput/filters/EditableFilter.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { QField, QToggle } from 'quasar'
+
+interface Props {
+  modelValue?: boolean
+}
+
+interface Emits {
+  (e: 'update:modelValue', value: boolean): void
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+function updateModelValue(value: boolean) {
+  emit('update:modelValue', value)
+}
+</script>
+<template>
+  <div class="select-wrapper">
+    <QField filled dense>
+      <template v-slot:control>
+        <QToggle
+          label="Zeige nur bearbeitebare Aktionen"
+          class="flex justify-between full-width"
+          left-label
+          color="primary"
+          :model-value="props.modelValue"
+          emit-value
+          @update:model-value="updateModelValue"
+        />
+      </template>
+    </QField>
+  </div>
+</template>
+<style lang="scss" scoped>
+.select-wrapper {
+  margin: 0 0 0.5rem 0;
+}
+</style>
