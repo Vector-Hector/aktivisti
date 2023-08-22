f8c915eaf18d45764d991da52eb6c8239cd62fb7
Peter <peter@ctrl.alt.coop>
2023-08-22 15:56:18 +0200

    CAC_FRONTEND-410 Refactor to use script setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,159 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+import NavigationSidebar from 'src/components/NavigationSidebar.vue'
+import { uiStore } from 'src/store/UiStore'
+import {
+  ErrorBus,
+  NOT_AUTHORIZED,
+  SESSION_INVALID,
+  NO_INTERNET,
+  USER_NOT_FOUND
+} from 'src/utils/errorBus'
+import {
+  QToolbar,
+  QBtn,
+  QPageContainer,
+  QLayout,
+  QHeader,
+  QToolbarTitle,
+  useQuasar
+} from 'quasar'
+import { ionArrowBack } from '@quasar/extras/ionicons-v5'
+import { IntervalDebouncer } from 'src/utils/debounce'
+import { apiClient } from 'src/api/ApiClient'
+import { configStore } from 'src/store/ConfigStore'
+import { getAuthStore } from 'src/store/AuthStore'
+import { userStore } from 'src/store/UserStore'
+import PageLoadingSpinner from 'components/PageLoadingSpinner.vue'
+import { VersionHealth } from 'src/api/model/ConfigDto'
+import { useRoute, useRouter } from 'vue-router'
+
+const authStore = getAuthStore()
+const $route = useRoute()
+const $router = useRouter()
+const $q = useQuasar()
+
+const initialized = ref(false)
+const transitionDirection = ref<string | null>(null)
+
+const currentDepth = computed(() => {
+  return $route.path.split('/').filter((item) => !!item).length
+})
+
+const sidebarExpanded = computed(() => {
+  return uiStore.getState().sidebarExpanded
+})
+
+watch($route, (to, from) => {
+  const toDepth = to.path.split('/').length
+  const fromDepth = from.path.split('/').length
+
+  if (toDepth === fromDepth) {
+    transitionDirection.value = null
+  } else {
+    transitionDirection.value = toDepth < fromDepth ? 'right' : 'left'
+  }
+})
+
+onMounted(async () => {
+  // Retrieve config
+  try {
+    const configRequest = await apiClient.config.get()
+    configStore.setServiceConfig(configRequest.payload.data)
+  } catch (e) {
+    ErrorBus.emit(NO_INTERNET)
+  }
+  // Check version health and show warnings / errors
+  switch (configStore.state.service_config.version_health) {
+    case VersionHealth.UNKNOWN:
+      $q.notify({
+        color: 'warning',
+        message:
+          'Diese App-Version ist unbekannt und wird nicht unterstützt. ' +
+          'Bitte lade eine neue Version aus offiziellen Quellen.'
+      })
+      break
+    case VersionHealth.OBSOLETE:
+      $q.notify({
+        color: 'negative',
+        message:
+          'Diese App-Version ist kritisch veraltet und wird nicht mehr unterstützt. ' +
+          'Du musst die Seite neu laden oder ein Update durchführen, ansonsten wird die App vermutlich Fehler produzieren.'
+      })
+      break
+    case VersionHealth.DEPRECATED:
+      $q.notify({
+        color: 'warning',
+        message:
+          'Es gibt eine neuere Version dieser App. Bitte führe ein Update durch.'
+      })
+      break
+  }
+  // hydrate profile on app start
+  if (authStore.isLoggedIn()) {
+    try {
+      const profileRequest = await apiClient.user.get('me', ['sub_association'])
+      const permissionRequest = await apiClient.userPermissions.list({
+        user: profileRequest.payload.data.id
+      })
+      userStore.setUser(profileRequest.payload.data)
+      userStore.setHomeAssociation(
+        profileRequest.payload.embedded?.sub_association?.[0] ?? null
+      )
+      userStore.setPermissions(permissionRequest.payload.data)
+    } catch (error: any) {
+      if (error.response?.status === 403) {
+        authStore.clear()
+      } else {
+        ErrorBus.emit(NO_INTERNET)
+      }
+    }
+  }
+  initialized.value = true
+
+  const debouncer = new IntervalDebouncer()
+  ErrorBus.on(SESSION_INVALID, (message: string) => {
+    debouncer.executeDebounced(() => {
+      $q.notify({
+        type: 'warning',
+        message: message
+      })
+      void $router.push({ name: 'login' })
+    })
+  })
+  ErrorBus.on(NOT_AUTHORIZED, (message: string) => {
+    $q.notify({
+      type: 'negative',
+      message: message
+    })
+  })
+  ErrorBus.on(USER_NOT_FOUND, (message: string) => {
+    $q.notify({
+      type: 'negative',
+      message: message
+    })
+  })
+  ErrorBus.on(NO_INTERNET, () => {
+    debouncer.executeDebounced(() => {
+      $q.notify({
+        type: 'negative',
+        timeout: 5000,
+        message:
+          'Die Internetverbindung steht derzeit nicht zur Verfügung oder der ' +
+          'App-Dienst konnte nicht erreicht werden, versuche es später noch einmal'
+      })
+    })
+  })
+})
+
+function toggleSidebar() {
+  uiStore.toggleSidebar(!uiStore.getState().sidebarExpanded)
+}
+
+function backButton() {
+  $router.go(-1)
+}
+</script>
 <template>
   <PageLoadingSpinner v-if="!initialized" />
   <QLayout v-else view="hHr LpR ffr">
@@ -19,7 +175,7 @@
             {{ $route.meta.subtitle?.() }}
           </span>
         </QToolbarTitle>
-        <QToolbarTitle class="subtitle"> </QToolbarTitle>
+        <QToolbarTitle class="subtitle"></QToolbarTitle>
       </QToolbar>
     </QHeader>
     <NavigationSidebar />
@@ -30,181 +186,6 @@
     </QPageContainer>
   </QLayout>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import NavigationSidebar from 'src/components/NavigationSidebar.vue'
-import { uiStore } from 'src/store/UiStore'
-import AppTitle from 'src/components/AppTitle.vue'
-import {
-  ErrorBus,
-  NOT_AUTHORIZED,
-  SESSION_INVALID,
-  NO_INTERNET,
-  USER_NOT_FOUND
-} from 'src/utils/errorBus'
-import {
-  QToolbar,
-  QBtn,
-  QPageContainer,
-  QLayout,
-  QHeader,
-  QToolbarTitle
-} from 'quasar'
-import { ionArrowBack } from '@quasar/extras/ionicons-v5'
-import { IntervalDebouncer } from 'src/utils/debounce'
-import { apiClient } from 'src/api/ApiClient'
-import { configStore } from 'src/store/ConfigStore'
-import { getAuthStore } from 'src/store/AuthStore'
-import { userStore } from 'src/store/UserStore'
-import PageLoadingSpinner from 'components/PageLoadingSpinner.vue'
-import { VersionHealth } from 'src/api/model/ConfigDto'
-
-const authStore = getAuthStore()
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    PageLoadingSpinner,
-    AppTitle,
-    NavigationSidebar,
-    QToolbar,
-    QBtn,
-    QPageContainer,
-    QLayout,
-    QToolbarTitle,
-    QHeader
-  },
-  data() {
-    return {
-      initialized: false,
-      transitionDirection: null as string | null,
-      ionArrowBack
-    }
-  },
-  computed: {
-    currentDepth(): number {
-      return this.$route.path.split('/').filter((item) => !!item).length
-    },
-    sidebarExpanded() {
-      return uiStore.getState().sidebarExpanded
-    }
-  },
-  watch: {
-    $route(to, from) {
-      const toDepth = to.path.split('/').length
-      const fromDepth = from.path.split('/').length
-
-      if (toDepth === fromDepth) {
-        this.transitionDirection = null
-      } else {
-        this.transitionDirection = toDepth < fromDepth ? 'right' : 'left'
-      }
-    }
-  },
-  async created() {
-    // Retrieve config
-    try {
-      const configRequest = await apiClient.config.get()
-      configStore.setServiceConfig(configRequest.payload.data)
-    } catch (e) {
-      ErrorBus.emit(NO_INTERNET)
-    }
-    // Check version health and show warnings / errors
-    switch (configStore.state.service_config.version_health) {
-      case VersionHealth.UNKNOWN:
-        this.$q.notify({
-          color: 'warning',
-          message:
-            'Diese App-Version ist unbekannt und wird nicht unterstützt. ' +
-            'Bitte lade eine neue Version aus offiziellen Quellen.'
-        })
-        break
-      case VersionHealth.OBSOLETE:
-        this.$q.notify({
-          color: 'negative',
-          message:
-            'Diese App-Version ist kritisch veraltet und wird nicht mehr unterstützt. ' +
-            'Du musst die Seite neu laden oder ein Update durchführen, ansonsten wird die App vermutlich Fehler produzieren.'
-        })
-        break
-      case VersionHealth.DEPRECATED:
-        this.$q.notify({
-          color: 'warning',
-          message:
-            'Es gibt eine neuere Version dieser App. Bitte führe ein Update durch.'
-        })
-        break
-    }
-    // hydrate profile on app start
-    if (authStore.isLoggedIn()) {
-      try {
-        const profileRequest = await apiClient.user.get('me', [
-          'sub_association'
-        ])
-        const permissionRequest = await apiClient.userPermissions.list({
-          user: profileRequest.payload.data.id
-        })
-        userStore.setUser(profileRequest.payload.data)
-        userStore.setHomeAssociation(
-          profileRequest.payload.embedded?.sub_association?.[0] ?? null
-        )
-        userStore.setPermissions(permissionRequest.payload.data)
-      } catch (error: any) {
-        if (error.response?.status === 403) {
-          authStore.clear()
-        } else {
-          ErrorBus.emit(NO_INTERNET)
-        }
-      }
-    }
-    this.initialized = true
-  },
-  mounted() {
-    const debouncer = new IntervalDebouncer()
-    ErrorBus.on(SESSION_INVALID, (message: string) => {
-      debouncer.executeDebounced(() => {
-        this.$q.notify({
-          type: 'warning',
-          message: message
-        })
-        void this.$router.push({ name: 'login' })
-      })
-    })
-    ErrorBus.on(NOT_AUTHORIZED, (message: string) => {
-      this.$q.notify({
-        type: 'negative',
-        message: message
-      })
-    })
-    ErrorBus.on(USER_NOT_FOUND, (message: string) => {
-      this.$q.notify({
-        type: 'negative',
-        message: message
-      })
-    })
-    ErrorBus.on(NO_INTERNET, () => {
-      debouncer.executeDebounced(() => {
-        this.$q.notify({
-          type: 'negative',
-          timeout: 5000,
-          message:
-            'Die Internetverbindung steht derzeit nicht zur Verfügung oder der ' +
-            'App-Dienst konnte nicht erreicht werden, versuche es später noch einmal'
-        })
-      })
-    })
-  },
-  methods: {
-    toggleSidebar() {
-      uiStore.toggleSidebar(!uiStore.getState().sidebarExpanded)
-    },
-    backButton() {
-      this.$router.go(-1)
-    }
-  }
-})
-</script>
 
 <style lang="scss" scoped>
 @import 'src/css/variables.scss';
