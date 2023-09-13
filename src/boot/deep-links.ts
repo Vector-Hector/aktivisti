89ea61adafeaafe7071b566ac83975cf64365b99
Jonatan Zint <jonatan@ctrl.alt.coop>
2023-09-13 15:28:08 +0200

    fix: base url is not available in runtime, use a simpler method to extract the path from incoming url

--- src/boot/deep-links.ts
+++ src/boot/deep-links.ts
@@ -3,7 +3,7 @@ import { App, URLOpenListenerEvent } from '@capacitor/app'
 
 export default boot(({ router }) => {
   void App.addListener('appUrlOpen', function (event: URLOpenListenerEvent) {
-    const slug = event.url.split(process.env.BASE_URL!).pop()
+    const slug = new URL(event.url).pathname
 
     // We only push to the route if there is a slug present
     if (slug) {
