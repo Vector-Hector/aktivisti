dbb6ce8cb7e1f0ef18add45fb18a6c8f5db1380a
Peter <peter@ctrl.alt.coop>
2024-01-12 22:15:47 +0100

    CAC_FRONTEND-397 Migrate EventAreaMetrics to script setup

--- src/pages/event-map/detail/area/metrics/EventAreaMetrics.vue
+++ src/pages/event-map/detail/area/metrics/EventAreaMetrics.vue
@@ -1,3 +1,91 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+import { EventMetricRecordDto } from 'src/api/model/EventMetricRecordDto'
+import { EventMetricDto } from 'src/api/model/EventMetricDto'
+import {
+  MetricValueMap,
+  trackingSessionStore
+} from 'src/store/TrackingSessionStore'
+import MetricsRow from 'src/components/MetricsRow.vue'
+import { uiStore } from 'src/store/UiStore'
+import { QBtn, QScrollArea } from 'quasar'
+import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
+import { useEventAreaMetricsComposable } from 'pages/event-map/detail/area/metrics/EventAreaMetricsMixin'
+import { useRoute } from 'vue-router'
+import { apiClient } from 'src/api/ApiClient'
+
+interface Props {
+  houseNumber: string
+  street: string
+  areaId: string
+}
+
+const props = defineProps<Props>()
+
+const $route = useRoute()
+const { eventArea } = useEventDetailStore()
+const { address } = useEventAreaMetricsComposable(props)
+
+const metricRecords = ref<EventMetricRecordDto[]>([])
+const metrics = ref<EventMetricDto[]>([])
+
+onMounted(async () => {
+  const metricsRequest = await apiClient.eventMetricRecords.list(
+    { event: eventArea.value.event },
+    ['metric']
+  )
+  metricRecords.value = metricsRequest.payload.data
+  metrics.value = metricsRequest.payload.embedded.metric
+})
+
+const metricValues = computed({
+  get() {
+    const storedValues = trackingSessionStore.getMetricsForAddress(
+      eventArea.value.id!,
+      address.value!
+    )
+    if (storedValues) {
+      return storedValues
+    } else {
+      const defaultObject: MetricValueMap = {}
+      for (const metricRecord of metricRecords.value) {
+        defaultObject[metricRecord.id] = 0
+      }
+      return defaultObject
+    }
+  },
+  async set(newMetrics: MetricValueMap) {
+    await trackingSessionStore.updateMetricsForAddress(
+      eventArea.value.id!,
+      address.value!,
+      newMetrics
+    )
+  }
+})
+
+watch(
+  () => $route.params,
+  (params) => {
+    uiStore.updateActiveElements({
+      // @ts-ignore
+      houseNumber: `${params.street} ${params.houseNumber}`
+    })
+  },
+  { immediate: true }
+)
+
+function getMetricForId(findId: number): EventMetricDto | undefined {
+  return metrics.value.find(({ id }) => id === findId)
+}
+
+function updateMetricValue(metricRecordId: number, value: string) {
+  metricValues.value = {
+    ...metricValues.value,
+    [metricRecordId]: value
+  }
+}
+</script>
+
 <template>
   <QScrollArea class="d-flex flex-fill column q-py-sm">
     <div class="create-lead">
@@ -19,111 +107,6 @@
     <p v-else>Für diese Aktion wurden keine Ergebnisse definiert</p>
   </QScrollArea>
 </template>
-
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { EventMetricRecordDto } from 'src/api/model/EventMetricRecordDto'
-import { EventMetricDto } from 'src/api/model/EventMetricDto'
-import {
-  MetricValueMap,
-  trackingSessionStore
-} from 'src/store/TrackingSessionStore'
-import MetricsRow from 'src/components/MetricsRow.vue'
-import { uiStore } from 'src/store/UiStore'
-import { QBtn, QScrollArea } from 'quasar'
-import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
-import { useEventAreaMetricsComposable } from 'pages/event-map/detail/area/metrics/EventAreaMetricsMixin'
-
-export default defineComponent({
-  name: 'EventAreaMetrics',
-  props: {
-    houseNumber: {
-      type: String as PropType<string>,
-      required: true
-    },
-    street: {
-      type: String as PropType<string>,
-      required: true
-    },
-    areaId: {
-      type: String as PropType<string>,
-      required: true
-    }
-  },
-  components: {
-    MetricsRow,
-    QBtn,
-    QScrollArea
-  },
-  setup(props) {
-    const { eventArea } = useEventDetailStore()
-    const { address } = useEventAreaMetricsComposable(props)
-    return { eventArea, address }
-  },
-  data() {
-    return {
-      metricRecords: [] as EventMetricRecordDto[],
-      metrics: [] as EventMetricDto[]
-    }
-  },
-  computed: {
-    metricValues: {
-      get(): MetricValueMap {
-        const storedValues = trackingSessionStore.getMetricsForAddress(
-          this.eventArea.id!,
-          this.address!
-        )
-        if (storedValues) {
-          return storedValues
-        } else {
-          const defaultObject: MetricValueMap = {}
-          for (const metricRecord of this.metricRecords) {
-            defaultObject[metricRecord.id] = 0
-          }
-          return defaultObject
-        }
-      },
-      async set(metrics: MetricValueMap) {
-        await trackingSessionStore.updateMetricsForAddress(
-          this.eventArea.id!,
-          this.address!,
-          metrics
-        )
-      }
-    }
-  },
-  watch: {
-    '$route.params': {
-      handler(params) {
-        uiStore.updateActiveElements({
-          // @ts-ignore
-          houseNumber: `${params.street} ${params.houseNumber}`
-        })
-      },
-      immediate: true
-    }
-  },
-  async created() {
-    const metricsRequest = await this.$apiClient.eventMetricRecords.list(
-      { event: this.eventArea.event },
-      ['metric']
-    )
-    this.metricRecords = metricsRequest.payload.data
-    this.metrics = metricsRequest.payload.embedded.metric
-  },
-  methods: {
-    getMetricForId(findId: number): EventMetricDto | undefined {
-      return this.metrics.find(({ id }) => id === findId)
-    },
-    updateMetricValue(metricRecordId: number, value: string) {
-      this.metricValues = {
-        ...this.metricValues,
-        [metricRecordId]: value
-      }
-    }
-  }
-})
-</script>
 
 <style lang="scss" scoped>
 label {
