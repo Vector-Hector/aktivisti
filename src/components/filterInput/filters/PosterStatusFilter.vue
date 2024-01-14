73c49f272695f2122517953c8a9b4ea25c0f5668
Peter <peter@ctrl.alt.coop>
2024-01-14 18:35:17 +0100

    CAC_FRONTEND-397 Migrate PosterStatusFilter to script setup

--- src/components/filterInput/filters/PosterStatusFilter.vue
+++ src/components/filterInput/filters/PosterStatusFilter.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import FilterInput from 'components/filterInput/FilterInput.vue'
+import { PosterStatus, posterStatusOptions } from 'src/api/model/PosterDto'
+
+const ALL_STATES = 'all_states'
+
+interface Props {
+  modelValue?: PosterStatus | typeof ALL_STATES
+}
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: () => ALL_STATES
+})
+
+interface Emits {
+  (e: 'update:modelValue', modelValue: string | undefined): void
+}
+const emit = defineEmits<Emits>()
+
+const extendedOptions = computed(() => {
+  return [
+    {
+      key: ALL_STATES,
+      label: 'Alle Status'
+    },
+    ...posterStatusOptions
+  ]
+})
+
+function updateModelValue(value: string) {
+  emit('update:modelValue', value !== ALL_STATES ? value : undefined)
+}
+</script>
 <template>
   <FilterInput
     label="Plakat Status"
-    :model-value="modelValue"
+    :model-value="props.modelValue"
     @update:model-value="updateModelValue"
     :options="extendedOptions"
     emit-value
@@ -10,43 +43,3 @@
     option-label="label"
   />
 </template>
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import FilterInput from 'components/filterInput/FilterInput.vue'
-import { PosterStatus, posterStatusOptions } from 'src/api/model/PosterDto'
-
-const ALL_STATES = 'all_states'
-
-export default defineComponent({
-  name: 'PosterStatusFilter',
-  components: { FilterInput },
-  props: {
-    modelValue: {
-      type: String as PropType<PosterStatus>,
-      default: ALL_STATES
-    }
-  },
-  data() {
-    return {
-      posterStatusOptions
-    }
-  },
-  emits: ['update:modelValue'],
-  computed: {
-    extendedOptions(): { key: string; label: string }[] {
-      return [
-        {
-          key: ALL_STATES,
-          label: 'Alle Status'
-        },
-        ...posterStatusOptions
-      ]
-    }
-  },
-  methods: {
-    updateModelValue(value: string) {
-      this.$emit('update:modelValue', value !== ALL_STATES ? value : undefined)
-    }
-  }
-})
-</script>
