ea5efade790519aff5fb053a72abbf5253ac7676
Peter <peter@ctrl.alt.coop>
2024-01-17 09:58:28 +0100

    Adjust order of component blocks

--- src/pages/edit-event/geometry/EditEventGeometry.vue
+++ src/pages/edit-event/geometry/EditEventGeometry.vue
@@ -1,175 +1,3 @@
-<template>
-  <div class="edit-event-geometry container">
-    <div class="location-select">
-      <h2 class="headline">
-        {{ locationHeadline }}
-      </h2>
-      <LocationSelect
-        v-model:location="event.location"
-        v-model:location-description="event.location_description"
-        :error="errors.location?.[0]"
-      />
-    </div>
-    <template v-if="event.event_type !== EventTypes.GENERIC">
-      <div class="area-drawing">
-        <h2 class="headline">Gebiete</h2>
-        <QTable
-          :loading="isLoading"
-          loading-label="Lade Daten zu Gebieten"
-          :auto-layout="true"
-          flat
-          dense
-          :value="eventAreas"
-          :columns="columns"
-          :rows="eventAreas"
-          virtual-scroll
-          hide-pagination
-          :rows-per-page-options="[0]"
-          class="editable-cells-table overflow-hidden q-my-sm"
-          edit-mode="cell"
-          no-data-label="Noch keine Gebiete gezeichnet"
-          @cell-edit-complete="updateArea($event.data)"
-        >
-          <template v-slot:header="props">
-            <QTr :props="props">
-              <QTh
-                v-for="col in props.cols"
-                :key="col.name"
-                :props="props"
-                class="table-header"
-              >
-                {{ col.label }}
-              </QTh>
-            </QTr>
-          </template>
-          <template v-slot:body="props">
-            <QTr>
-              <QTd key="color" :props="props">
-                <QBtn
-                  unelevated
-                  round
-                  dense
-                  size="sm"
-                  :style="{
-                    'background-color': props.row.color
-                  }"
-                  :color="props.row.color"
-                >
-                  <QPopupProxy>
-                    <QColor
-                      no-header
-                      no-footer
-                      default-view="palette"
-                      :model-value="props.row.color"
-                      @update:modelValue="handleUpdateColor(props.row, $event)"
-                    />
-                  </QPopupProxy>
-                </QBtn>
-              </QTd>
-              <QTd key="name" :props="props">
-                <div>
-                  {{ props.row.name }}
-                  <QBtn
-                    :icon="ionPencil"
-                    flat
-                    round
-                    dense
-                    size="sm"
-                    color="grey-6"
-                  />
-                  <QPopupEdit
-                    v-model="props.row.name"
-                    @save="(value) => updateArea({ ...props.row, name: value })"
-                    :auto-save="true"
-                    v-slot="scope"
-                  >
-                    <QInput
-                      v-model="scope.value"
-                      @keyup.enter="scope.set"
-                      dense
-                      autofocus
-                    />
-                  </QPopupEdit>
-                </div>
-              </QTd>
-              <QTd key="details" :props="props">
-                <QSpinnerPuff
-                  v-if="updatingAreaFeatureIds.has(props.row.feature_id)"
-                  class="progress-spinner"
-                />
-                <div
-                  v-else-if="props.row.feature_id in eventAreasWithError"
-                  class="error-indicator"
-                >
-                  <QIcon
-                    class="event-area-error-icon"
-                    color="negative"
-                    size="sm"
-                    :name="ionAlertCircleOutline"
-                    aria-label="Fehlerindikator für Gebiet"
-                  >
-                  </QIcon>
-                  <QTooltip v-model="showing">
-                    {{ eventAreasWithError[props.row.feature_id] }}
-                  </QTooltip>
-                </div>
-                <span v-else-if="event.event_type === EventTypes.POSTERS">
-                  {{ props.row.poster_count }}
-                </span>
-                <span v-else>
-                  {{
-                    props.row.area_details?.streets?.reduce(
-                      (acc, item) => acc + item.addresses.length,
-                      0
-                    ) ?? 0
-                  }}
-                </span>
-              </QTd>
-
-              <QTd key="actions" :props="props">
-                <QSpinnerPuff v-if="deletingAreaIds.has(props.row.id)" />
-                <QBtn
-                  v-else
-                  dense
-                  round
-                  flat
-                  color="grey-6"
-                  :icon="ionTrash"
-                  @click="deleteAreaByFeatureId(props.row.feature_id)"
-                />
-              </QTd>
-            </QTr>
-          </template>
-        </QTable>
-        <div class="buttons">
-          <QBtn
-            :icon="ionCopyOutline"
-            dense
-            size="md"
-            label="Gebiete übernehmen"
-            @click="openAdoptAreasModal"
-          />
-          <QBtn
-            color="primary"
-            class="add-area-button"
-            :icon="ionCreateOutline"
-            dense
-            size="md"
-            label="Gebiet zeichnen"
-            @click="startDrawArea"
-          />
-        </div>
-      </div>
-    </template>
-  </div>
-  <SidebarBottomStepNavigation
-    @close="abort"
-    @forward="next"
-    @back="back"
-    :last="stepControls.isLastStep.value"
-  />
-</template>
-
 <script lang="ts">
 import { defineComponent, inject } from 'vue'
 import {
@@ -417,6 +245,178 @@ export default defineComponent({
   }
 })
 </script>
+
+<template>
+  <div class="edit-event-geometry container">
+    <div class="location-select">
+      <h2 class="headline">
+        {{ locationHeadline }}
+      </h2>
+      <LocationSelect
+        v-model:location="event.location"
+        v-model:location-description="event.location_description"
+        :error="errors.location?.[0]"
+      />
+    </div>
+    <template v-if="event.event_type !== EventTypes.GENERIC">
+      <div class="area-drawing">
+        <h2 class="headline">Gebiete</h2>
+        <QTable
+          :loading="isLoading"
+          loading-label="Lade Daten zu Gebieten"
+          :auto-layout="true"
+          flat
+          dense
+          :value="eventAreas"
+          :columns="columns"
+          :rows="eventAreas"
+          virtual-scroll
+          hide-pagination
+          :rows-per-page-options="[0]"
+          class="editable-cells-table overflow-hidden q-my-sm"
+          edit-mode="cell"
+          no-data-label="Noch keine Gebiete gezeichnet"
+          @cell-edit-complete="updateArea($event.data)"
+        >
+          <template v-slot:header="props">
+            <QTr :props="props">
+              <QTh
+                v-for="col in props.cols"
+                :key="col.name"
+                :props="props"
+                class="table-header"
+              >
+                {{ col.label }}
+              </QTh>
+            </QTr>
+          </template>
+          <template v-slot:body="props">
+            <QTr>
+              <QTd key="color" :props="props">
+                <QBtn
+                  unelevated
+                  round
+                  dense
+                  size="sm"
+                  :style="{
+                    'background-color': props.row.color
+                  }"
+                  :color="props.row.color"
+                >
+                  <QPopupProxy>
+                    <QColor
+                      no-header
+                      no-footer
+                      default-view="palette"
+                      :model-value="props.row.color"
+                      @update:modelValue="handleUpdateColor(props.row, $event)"
+                    />
+                  </QPopupProxy>
+                </QBtn>
+              </QTd>
+              <QTd key="name" :props="props">
+                <div>
+                  {{ props.row.name }}
+                  <QBtn
+                    :icon="ionPencil"
+                    flat
+                    round
+                    dense
+                    size="sm"
+                    color="grey-6"
+                  />
+                  <QPopupEdit
+                    v-model="props.row.name"
+                    @save="(value) => updateArea({ ...props.row, name: value })"
+                    :auto-save="true"
+                    v-slot="scope"
+                  >
+                    <QInput
+                      v-model="scope.value"
+                      @keyup.enter="scope.set"
+                      dense
+                      autofocus
+                    />
+                  </QPopupEdit>
+                </div>
+              </QTd>
+              <QTd key="details" :props="props">
+                <QSpinnerPuff
+                  v-if="updatingAreaFeatureIds.has(props.row.feature_id)"
+                  class="progress-spinner"
+                />
+                <div
+                  v-else-if="props.row.feature_id in eventAreasWithError"
+                  class="error-indicator"
+                >
+                  <QIcon
+                    class="event-area-error-icon"
+                    color="negative"
+                    size="sm"
+                    :name="ionAlertCircleOutline"
+                    aria-label="Fehlerindikator für Gebiet"
+                  >
+                  </QIcon>
+                  <QTooltip v-model="showing">
+                    {{ eventAreasWithError[props.row.feature_id] }}
+                  </QTooltip>
+                </div>
+                <span v-else-if="event.event_type === EventTypes.POSTERS">
+                  {{ props.row.poster_count }}
+                </span>
+                <span v-else>
+                  {{
+                    props.row.area_details?.streets?.reduce(
+                      (acc, item) => acc + item.addresses.length,
+                      0
+                    ) ?? 0
+                  }}
+                </span>
+              </QTd>
+
+              <QTd key="actions" :props="props">
+                <QSpinnerPuff v-if="deletingAreaIds.has(props.row.id)" />
+                <QBtn
+                  v-else
+                  dense
+                  round
+                  flat
+                  color="grey-6"
+                  :icon="ionTrash"
+                  @click="deleteAreaByFeatureId(props.row.feature_id)"
+                />
+              </QTd>
+            </QTr>
+          </template>
+        </QTable>
+        <div class="buttons">
+          <QBtn
+            :icon="ionCopyOutline"
+            dense
+            size="md"
+            label="Gebiete übernehmen"
+            @click="openAdoptAreasModal"
+          />
+          <QBtn
+            color="primary"
+            class="add-area-button"
+            :icon="ionCreateOutline"
+            dense
+            size="md"
+            label="Gebiet zeichnen"
+            @click="startDrawArea"
+          />
+        </div>
+      </div>
+    </template>
+  </div>
+  <SidebarBottomStepNavigation
+    @close="abort"
+    @forward="next"
+    @back="back"
+    :last="stepControls.isLastStep.value"
+  />
+</template>
 
 <style lang="scss" scoped>
 @import 'src/css/quasar.variables';
