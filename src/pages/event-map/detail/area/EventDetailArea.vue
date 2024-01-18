04d00b13a75f6a26ee991d6fd98e25b11a1f0884
Peter <peter@ctrl.alt.coop>
2024-01-18 10:17:09 +0100

    CAC_FRONTEND-397 Migrate EventDetailArea to script setup

--- src/pages/event-map/detail/area/EventDetailArea.vue
+++ src/pages/event-map/detail/area/EventDetailArea.vue
@@ -1,20 +1,11 @@
+<!--FIXME(peter) 2023/12/12 The composition API doesn't support `beforeRouteEnter` so far so this a workaround
+      see https://github.com/vuejs/rfcs/discussions/302-->
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { apiClient } from 'src/api/ApiClient'
-import { eventDetailStore } from 'src/store/EventDetailStore'
-import Timeout = NodeJS.Timeout
-import { EventTypes } from 'src/api/model/EventTypes'
-import { NavigationGuardNext, RouteLocation } from 'vue-router'
-import { uiStore } from 'src/store/UiStore'
-import { UNDEFINED_POSTER_AREA } from 'pages/event-map/detail/area/posters/detail/EventDetailPosterDetail.vue'
-import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
-
-async function updateRoute(
-  to: RouteLocation,
-  from: RouteLocation,
-  next: NavigationGuardNext
-) {
-  const { areaId } = to.params
+/**
+ * This is an exact copy of updateRoute(). Which is a result of the FIXME.
+ */
+async function updateRouteCopy(params: RouteParams) {
+  const { areaId } = params
   if (areaId === UNDEFINED_POSTER_AREA) {
     // the special undefined route is for posters that are not assigned to an area
     eventDetailStore.setEventArea(null)
@@ -22,10 +13,9 @@ async function updateRoute(
     uiStore.updateActiveElements({
       eventArea: 'Undefiniertes Gebiet'
     })
-    next()
   } else {
     const response = await apiClient.eventAreas.get(
-      to.params.areaId as string,
+      params.areaId as string,
       [],
       { show_permissions: true }
     )
@@ -34,47 +24,81 @@ async function updateRoute(
     uiStore.updateActiveElements({
       eventArea: `Aktionsgebiet ${response.payload.data.name}`
     })
+  }
+}
+export default defineComponent({
+  async beforeRouteEnter(to, from, next) {
+    await updateRouteCopy(to.params)
     next()
+  }
+})
+</script>
+
+<script setup lang="ts">
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
+import { apiClient } from 'src/api/ApiClient'
+import { eventDetailStore } from 'src/store/EventDetailStore'
+import Timeout = NodeJS.Timeout
+import { EventTypes } from 'src/api/model/EventTypes'
+import { RouteParams, onBeforeRouteUpdate } from 'vue-router'
+import { uiStore } from 'src/store/UiStore'
+import { UNDEFINED_POSTER_AREA } from 'pages/event-map/detail/area/posters/detail/EventDetailPosterDetail.vue'
+import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
+
+const { event, eventArea } = useEventDetailStore()
+
+const nextPoll = ref<Timeout | null>(null)
+
+async function updateRoute(params: RouteParams) {
+  const { areaId } = params
+  if (areaId === UNDEFINED_POSTER_AREA) {
+    // the special undefined route is for posters that are not assigned to an area
+    eventDetailStore.setEventArea(null)
+    eventDetailStore.setEventAreaPermissions(null)
+    uiStore.updateActiveElements({
+      eventArea: 'Undefiniertes Gebiet'
+    })
+  } else {
+    const response = await apiClient.eventAreas.get(
+      params.areaId as string,
+      [],
+      { show_permissions: true }
+    )
+    eventDetailStore.setEventArea(response.payload.data)
+    eventDetailStore.setEventAreaPermissions(response.payload.permissions)
+    uiStore.updateActiveElements({
+      eventArea: `Aktionsgebiet ${response.payload.data.name}`
+    })
   }
 }
 
-export default defineComponent({
-  name: 'EventDetailArea',
-  beforeRouteEnter: updateRoute,
-  beforeRouteUpdate: updateRoute,
-  setup() {
-    const { event, eventArea } = useEventDetailStore()
-    return { event, eventArea }
-  },
-  data() {
-    return {
-      nextPoll: null as Timeout | null
-    }
-  },
-  async created() {
-    // there are no completion notes in poster events
-    if (this.event.event_type !== EventTypes.POSTERS) {
-      await this.pollForCompletionNotes()
-    }
-  },
-  unmounted() {
-    if (this.nextPoll !== null) {
-      clearTimeout(this.nextPoll)
-    }
-  },
-  methods: {
-    async pollForCompletionNotes() {
-      eventDetailStore.addCompletionNotes(
-        (
-          await this.$apiClient.completionNotes.list({
-            event_area: this.eventArea.id
-          })
-        ).payload.data
-      )
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      this.nextPoll = setTimeout(() => this.pollForCompletionNotes(), 5000)
-    }
+onMounted(async () => {
+  if (event.value.event_type !== EventTypes.POSTERS) {
+    await pollForCompletionNotes()
   }
+})
+
+onUnmounted(() => {
+  if (nextPoll.value !== null) {
+    clearTimeout(nextPoll.value)
+  }
+})
+
+async function pollForCompletionNotes() {
+  eventDetailStore.addCompletionNotes(
+    (
+      await apiClient.completionNotes.list({
+        event_area: eventArea.value.id
+      })
+    ).payload.data
+  )
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  nextPoll.value = setTimeout(() => pollForCompletionNotes(), 5000)
+}
+
+onBeforeRouteUpdate(async (to, from, next) => {
+  await updateRoute(to.params)
+  next()
 })
 </script>
 
