da3c082c05beb3c9a8d5869a763d7a5f6d221ab8
Peter <peter@ctrl.alt.coop>
2024-01-18 13:48:16 +0100

    Change order of components blocks

--- src/components/InfiniteList.vue
+++ src/components/InfiniteList.vue
@@ -1,30 +1,3 @@
-<template>
-  <QInfiniteScroll
-    v-if="items.length > 0"
-    @load="(index, done) => $emit('load', index, done)"
-    :disable="disable"
-  >
-    <QList>
-      <slot
-        name="item"
-        :item="item"
-        v-for="(item, index) in items"
-        :key="index"
-      >
-        {{ item }}
-      </slot>
-    </QList>
-    <template v-slot:loading>
-      <div class="row justify-center q-my-md">
-        <QSpinnerDots color="primary" size="40px" />
-      </div>
-    </template>
-  </QInfiniteScroll>
-  <div v-else class="empty-list-placeholder">
-    <slot name="emptyList"> Keine Einträge gefunden</slot>
-  </div>
-</template>
-
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { QInfiniteScroll, QList, QSpinnerDots } from 'quasar'
@@ -57,6 +30,34 @@ export default defineComponent({
   }
 })
 </script>
+
+<template>
+  <QInfiniteScroll
+    v-if="items.length > 0"
+    @load="(index, done) => $emit('load', index, done)"
+    :disable="disable"
+  >
+    <QList>
+      <slot
+        name="item"
+        :item="item"
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        {{ item }}
+      </slot>
+    </QList>
+    <template v-slot:loading>
+      <div class="row justify-center q-my-md">
+        <QSpinnerDots color="primary" size="40px" />
+      </div>
+    </template>
+  </QInfiniteScroll>
+  <div v-else class="empty-list-placeholder">
+    <slot name="emptyList"> Keine Einträge gefunden</slot>
+  </div>
+</template>
+
 <style lang="scss" scoped>
 .empty-list-placeholder {
   margin: 1rem 0;
