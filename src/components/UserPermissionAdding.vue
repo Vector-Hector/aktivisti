374bfc1de20f534c181d2c3b779f1a25111b1202
Peter <peter@ctrl.alt.coop>
2024-01-19 13:35:39 +0100

    Migrate UserPermissionAdding to sript setup

--- src/components/UserPermissionAdding.vue
+++ src/components/UserPermissionAdding.vue
@@ -1,87 +1,73 @@
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import {
   ContentTypeOption,
   ExtendedPermissionTypeOption
 } from 'pages/ManageUsers.vue'
-import { QBtn, QInput, QSelect } from 'quasar'
+import { QBtn, QInput, QSelect, useQuasar } from 'quasar'
 import { ionChevronDown } from '@quasar/extras/ionicons-v5'
 import { ErrorBus, USER_NOT_FOUND } from 'src/utils/errorBus'
+import { apiClient } from 'src/api/ApiClient'
 
-export default defineComponent({
-  name: 'UserPermissionAdding',
-  props: {
-    /**
-     * Permission that could be assigned to user.
-     */
-    permissionTypeConditionalOptions: {
-      type: Object as PropType<ExtendedPermissionTypeOption[]>,
-      required: true
-    },
-    /**
-     * The content_type for which the list should be generated.
-     * see `api/v1/content-types/`
-     */
-    contentType: {
-      type: Object as PropType<ContentTypeOption>,
-      required: true
-    },
-    /**
-     * The ID of an object that has the type of the ContentType `contentType`.
-     */
-    objectId: {
-      type: Number,
-      required: true
-    }
-  },
-  components: {
-    QBtn,
-    QInput,
-    QSelect
-  },
-  data() {
-    return {
-      ionChevronDown,
-      username: '' as string,
-      selectedPermissionType: this.permissionTypeConditionalOptions.find(
-        ({ inactive }) => !inactive
-      )
-    }
-  },
-  emit: ['submit'],
-  methods: {
-    async handleSubmission() {
-      const newUserObjectPermissions = {
-        user: this.username,
-        object_pk: this.objectId.toString(),
-        content_type: this.contentType.id,
-        permission_codename: this.selectedPermissionType?.key
+interface Props {
+  /**
+   * Permission that could be assigned to user.
+   */
+  permissionTypeConditionalOptions: ExtendedPermissionTypeOption[]
+  /**
+   * The content_type for which the list should be generated.
+   * see `api/v1/content-types/`
+   */
+  contentType: ContentTypeOption
+  /**
+   * The ID of an object that has the type of the ContentType `contentType`.
+   */
+  objectId: number
+}
+const props = defineProps<Props>()
+
+const $q = useQuasar()
+
+const username = ref('')
+const selectedPermissionType = ref(
+  props.permissionTypeConditionalOptions.find(({ inactive }) => !inactive)
+)
+
+interface Emits {
+  (e: 'submit'): void
+}
+const emit = defineEmits<Emits>()
+
+async function handleSubmission() {
+  const newUserObjectPermissions = {
+    user: username.value,
+    object_pk: props.objectId.toString(),
+    content_type: props.contentType.id,
+    permission_codename: selectedPermissionType.value?.key
+  }
+  try {
+    await apiClient.userPermissions.create(newUserObjectPermissions)
+    emit('submit')
+  } catch (e) {
+    if (apiClient.isApiClientError(e) && e.response) {
+      const { status, data } = e.response
+      if (status === 409) {
+        $q.notify({
+          color: 'info',
+          message: 'Die Benutzer*in wurde bereits zur Liste hinzugefügt'
+        })
+      } else if (status === 400 && data.user) {
+        ErrorBus.emit(USER_NOT_FOUND, 'Benutzer*in nicht gefunden.')
       }
-      try {
-        await this.$apiClient.userPermissions.create(newUserObjectPermissions)
-        this.$emit('submit')
-      } catch (e) {
-        if (this.$apiClient.isApiClientError(e) && e.response) {
-          const { status, data } = e.response
-          if (status === 409) {
-            this.$q.notify({
-              color: 'info',
-              message: 'Die Benutzer*in wurde bereits zur Liste hinzugefügt'
-            })
-          } else if (status === 400 && data.user) {
-            ErrorBus.emit(USER_NOT_FOUND, 'Benutzer*in nicht gefunden.')
-          }
-        } else {
-          this.$q.notify({
-            color: 'negative',
-            message:
-              'Beim Hinzufügen der Benutzer*in ist ein unbekannter Fehler aufgetreten'
-          })
-        }
-      }
+    } else {
+      $q.notify({
+        color: 'negative',
+        message:
+          'Beim Hinzufügen der Benutzer*in ist ein unbekannter Fehler aufgetreten'
+      })
     }
   }
-})
+}
 </script>
 
 <template>
