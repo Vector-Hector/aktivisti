00adbdfdab9940d04a53aad61d2d8fa13fd97bca
Peter <peter@ctrl.alt.coop>
2024-01-19 15:43:37 +0100

    Change order of Component blocks

--- src/components/modals/ChangePasswordDialog.vue
+++ src/components/modals/ChangePasswordDialog.vue
@@ -1,59 +1,3 @@
-<template>
-  <QDialog ref="dialog" @hide="onDialogHide">
-    <QCard class="change-email-dialog">
-      <QToolbar>
-        <QToolbarTitle>Passwort ändern</QToolbarTitle>
-      </QToolbar>
-      <QForm @submit="onOk">
-        <QCardSection>
-          <p>
-            Hier kannst du eine neues Passwort eingeben. Gib dazu dein aktuelles
-            und das neue Passwort ein
-          </p>
-          <PasswordInput
-            label="Aktuelles Passwort"
-            v-model="oldPassword"
-            :minlength="8"
-            :rules="[$validationRules.isRequired]"
-            :error-message="errors.old_password?.[0]"
-            :error="!!errors.old_password?.length"
-          />
-          <PasswordInput
-            label="Neues Passwort"
-            v-model="newPassword"
-            :minlength="8"
-            :rules="[$validationRules.isRequired]"
-            :error-message="errors.new_password?.[0]"
-            :error="!!errors.new_password?.length"
-          />
-          <PasswordInput
-            label="Neues Passwort bestätigen"
-            v-model="newPasswordConfirm"
-            :minlength="8"
-            :rules="[$validationRules.isRequired, passwordMatch]"
-          />
-        </QCardSection>
-        <QCardActions align="right">
-          <QBtn
-            flat
-            :disabled="isSubmitting"
-            color="primary"
-            label="Abbrechen"
-            @click="onDialogHide"
-          />
-          <QBtn
-            flat
-            :disabled="isSubmitting"
-            color="primary"
-            label="OK"
-            type="submit"
-          />
-        </QCardActions>
-      </QForm>
-    </QCard>
-  </QDialog>
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue'
 import {
@@ -143,5 +87,61 @@ export default defineComponent({
   }
 })
 </script>
+
+<template>
+  <QDialog ref="dialog" @hide="onDialogHide">
+    <QCard class="change-email-dialog">
+      <QToolbar>
+        <QToolbarTitle>Passwort ändern</QToolbarTitle>
+      </QToolbar>
+      <QForm @submit="onOk">
+        <QCardSection>
+          <p>
+            Hier kannst du eine neues Passwort eingeben. Gib dazu dein aktuelles
+            und das neue Passwort ein
+          </p>
+          <PasswordInput
+            label="Aktuelles Passwort"
+            v-model="oldPassword"
+            :minlength="8"
+            :rules="[$validationRules.isRequired]"
+            :error-message="errors.old_password?.[0]"
+            :error="!!errors.old_password?.length"
+          />
+          <PasswordInput
+            label="Neues Passwort"
+            v-model="newPassword"
+            :minlength="8"
+            :rules="[$validationRules.isRequired]"
+            :error-message="errors.new_password?.[0]"
+            :error="!!errors.new_password?.length"
+          />
+          <PasswordInput
+            label="Neues Passwort bestätigen"
+            v-model="newPasswordConfirm"
+            :minlength="8"
+            :rules="[$validationRules.isRequired, passwordMatch]"
+          />
+        </QCardSection>
+        <QCardActions align="right">
+          <QBtn
+            flat
+            :disabled="isSubmitting"
+            color="primary"
+            label="Abbrechen"
+            @click="onDialogHide"
+          />
+          <QBtn
+            flat
+            :disabled="isSubmitting"
+            color="primary"
+            label="OK"
+            type="submit"
+          />
+        </QCardActions>
+      </QForm>
+    </QCard>
+  </QDialog>
+</template>
 
 <style lang="scss" scoped></style>
