0551bf207946a9358d1cfb74f758e9e881829dfa
Peter <peter@ctrl.alt.coop>
2024-01-19 17:27:55 +0100

    Change order of component blocks

--- src/components/modals/ChangeUsernameDialog.vue
+++ src/components/modals/ChangeUsernameDialog.vue
@@ -1,53 +1,3 @@
-<template>
-  <QDialog ref="dialog">
-    <QCard class="change-username-dialog">
-      <QToolbar>
-        <QToolbarTitle>Benutzer*innenname ändern</QToolbarTitle>
-      </QToolbar>
-      <QForm @submit="onOk">
-        <QCardSection>
-          <p>
-            Hier kannst du eine neuen Benutzer*innenname festlegen. Dazu
-            brauchen wir noch einmal dein aktuelles Passwort. Nachdem du dein
-            Benutzer*innenname geändert hast, ist dein alter Benutzer*innenname
-            für alle anderen Benutzer*innen verfügbar.
-          </p>
-          <PasswordInput
-            label="Passwort"
-            v-model="password"
-            :minlength="8"
-            :rules="[$validationRules.isRequired]"
-            :error-message="errors.password?.[0]"
-            :error="!!errors.password?.length"
-          />
-          <QInput
-            label="Neuer Benutzer*innenname"
-            v-model="newUsername"
-            :rules="[$validationRules.isRequired]"
-            :error-message="errors.new_username?.[0]"
-            :error="!!errors.new_username?.length"
-          />
-        </QCardSection>
-        <QCardActions align="right">
-          <QBtn
-            flat
-            :disabled="isSubmitting"
-            color="primary"
-            label="Abbrechen"
-            @click="onDialogHide"
-          />
-          <QBtn
-            flat
-            :disabled="isSubmitting"
-            color="primary"
-            label="OK"
-            type="submit"
-          />
-        </QCardActions>
-      </QForm>
-    </QCard>
-  </QDialog>
-</template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import {
@@ -131,3 +81,54 @@ export default defineComponent({
   }
 })
 </script>
+
+<template>
+  <QDialog ref="dialog">
+    <QCard class="change-username-dialog">
+      <QToolbar>
+        <QToolbarTitle>Benutzer*innenname ändern</QToolbarTitle>
+      </QToolbar>
+      <QForm @submit="onOk">
+        <QCardSection>
+          <p>
+            Hier kannst du eine neuen Benutzer*innenname festlegen. Dazu
+            brauchen wir noch einmal dein aktuelles Passwort. Nachdem du dein
+            Benutzer*innenname geändert hast, ist dein alter Benutzer*innenname
+            für alle anderen Benutzer*innen verfügbar.
+          </p>
+          <PasswordInput
+            label="Passwort"
+            v-model="password"
+            :minlength="8"
+            :rules="[$validationRules.isRequired]"
+            :error-message="errors.password?.[0]"
+            :error="!!errors.password?.length"
+          />
+          <QInput
+            label="Neuer Benutzer*innenname"
+            v-model="newUsername"
+            :rules="[$validationRules.isRequired]"
+            :error-message="errors.new_username?.[0]"
+            :error="!!errors.new_username?.length"
+          />
+        </QCardSection>
+        <QCardActions align="right">
+          <QBtn
+            flat
+            :disabled="isSubmitting"
+            color="primary"
+            label="Abbrechen"
+            @click="onDialogHide"
+          />
+          <QBtn
+            flat
+            :disabled="isSubmitting"
+            color="primary"
+            label="OK"
+            type="submit"
+          />
+        </QCardActions>
+      </QForm>
+    </QCard>
+  </QDialog>
+</template>
