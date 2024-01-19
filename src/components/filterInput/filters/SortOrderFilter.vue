d2441863dbeec6413f6cdb6c7dae898f7166c038
Peter <peter@ctrl.alt.coop>
2024-01-19 14:53:57 +0100

    CAC_FRONTEND-397 Migrate SortOrderFilter to script setup

--- src/components/filterInput/filters/SortOrderFilter.vue
+++ src/components/filterInput/filters/SortOrderFilter.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import FilterInput from 'components/filterInput/FilterInput.vue'
 import { SortOption } from 'src/store/UserStore'
 
@@ -7,32 +7,24 @@ const SortOptionLabels = {
   [SortOption.START_DATE]: 'Datum (Beginn)',
   [SortOption.NAME]: 'Aktionsname'
 }
+interface Props {
+  modelValue?: string
+}
+const props = defineProps<Props>()
 
-export default defineComponent({
-  name: 'SortOrderFilter',
-  components: {
-    FilterInput
-  },
-  props: {
-    modelValue: {
-      type: String as PropType<string>
-    }
-  },
-  data() {
-    return {
-      sortOptions: Object.values(SortOption),
-      SortOptionLabels
-    }
-  },
-  emits: ['update:modelValue']
-})
+const sortOptions = ref(Object.values(SortOption))
+
+interface Emits {
+  (e: 'update:modelValue', string): void
+}
+const emit = defineEmits<Emits>()
 </script>
 
 <template>
   <FilterInput
     class="filter-dropdown"
-    :model-value="modelValue"
-    @update:model-value="(value) => this.$emit('update:modelValue', value)"
+    :model-value="props.modelValue"
+    @update:model-value="(value) => emit('update:modelValue', value)"
     input-debounce="0"
     label="Sortierung"
     :options="sortOptions"
