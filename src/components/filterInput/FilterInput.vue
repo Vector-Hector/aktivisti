440f6383018b954fe2fb236117daac82c50290ec
Peter <peter@ctrl.alt.coop>
2024-01-25 17:02:59 +0100

    CAC_FRONTEND-397 Migrate to script setup

--- src/components/filterInput/FilterInput.vue
+++ src/components/filterInput/FilterInput.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+import { ionChevronDown, ionClose } from '@quasar/extras/ionicons-v5'
+import { QSelect } from 'quasar'
+
+interface Props {
+  modelValue?: typeof QSelect.$props
+  //  TODO(peter@ctrl.alt.coop) modelValue should be required, but passed modelValue is sometimes undefined
+  //  It needs to be checked which passed modelValues are undefined and to fix this.
+}
+const props = defineProps<Props>()
+</script>
+
 <template>
   <QSelect
     :dropdownIcon="ionChevronDown"
     :clearIcon="ionClose"
     filled
-    :model-value="modelValue"
+    :model-value="props.modelValue"
     v-bind="$attrs"
   >
     <template v-for="(_, name) in $slots" v-slot:[name]="slotData">
@@ -11,28 +23,3 @@
     </template>
   </QSelect>
 </template>
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { ionChevronDown, ionClose } from '@quasar/extras/ionicons-v5'
-import { QSelect } from 'quasar'
-
-export default defineComponent({
-  name: 'FilterInput',
-  components: {
-    QSelect
-  },
-  props: {
-    modelValue: {
-      type: [Number, String, Array] as PropType<typeof QSelect.$props>
-      //  TODO(peter@ctrl.alt.coop) modelValue should be required, but passed modelValue is sometimes undefined
-      //  It needs to be checked which passed modelValues are undefined and to fix this.
-    }
-  },
-  data() {
-    return {
-      ionChevronDown,
-      ionClose
-    }
-  }
-})
-</script>
