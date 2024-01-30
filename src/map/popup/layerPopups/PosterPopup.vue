5a7342e40d0e43f3914985ebb773383e667fb458
Peter <peter@ctrl.alt.coop>
2024-01-30 15:19:48 +0100

    CAC_FRONTEND-397 Migrate PosterPopup to script setup

--- src/map/popup/layerPopups/PosterPopup.vue
+++ src/map/popup/layerPopups/PosterPopup.vue
@@ -1,26 +1,18 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import LayerPopup from 'src/map/popup/LayerPopup.vue'
 import { PosterDto } from 'src/api/model/PosterDto'
 import { QBtn } from 'quasar'
 import { UNDEFINED_POSTER_AREA } from 'pages/event-map/detail/area/posters/detail/EventDetailPosterDetail.vue'
 
-export default defineComponent({
-  name: 'PosterPopup',
-  components: { LayerPopup, QBtn },
-  data() {
-    return {
-      UNDEFINED_POSTER_AREA
-    }
-  },
-  methods: {
-    showPopup(poster: PosterDto) {
-      const { lng, lat } = poster.location
-      // @ts-ignore
-      this.$refs.layerPopup.showPopup(lng, lat, poster)
-    }
-  }
-})
+const layerPopup = ref<InstanceType<typeof LayerPopup> | null>(null)
+
+function showPopup(poster: PosterDto) {
+  const { lng, lat } = poster.location
+  layerPopup.value?.showPopup(lng, lat, poster)
+}
+
+defineExpose({ showPopup })
 </script>
 
 <template>
