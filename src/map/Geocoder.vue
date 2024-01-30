e867aca8dbfad3101060c87011795a30802b1b53
Peter <peter@ctrl.alt.coop>
2024-01-30 16:07:23 +0100

    CAC_FRONTEND-397 Migrate Geocoder to script setup

--- src/map/Geocoder.vue
+++ src/map/Geocoder.vue
@@ -1,13 +1,5 @@
-<script lang="ts">
-import {
-  defineComponent,
-  inject,
-  onMounted,
-  onUnmounted,
-  PropType,
-  ref
-} from 'vue'
-//@ts-ignore
+<script setup lang="ts">
+import { inject, onMounted, onUnmounted, ref } from 'vue'
 import MaplibreGeocoder from '@maplibre/maplibre-gl-geocoder'
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css'
 import { forwardGeocode, reverseGeocode } from 'src/utils/map'
@@ -15,86 +7,65 @@ import { GeocodeResult } from 'src/types/GeocodeResult'
 import maplibregl, { MarkerOptions } from 'maplibre-gl'
 import { MapKey } from 'src/types/keys'
 
-export default defineComponent({
-  name: 'Geocoder',
-  props: {
-    countries: {
-      type: Array as PropType<string[]> | null,
-      default: null
-    },
-    markerOptions: {
-      type: Object as PropType<MarkerOptions | boolean>,
-      required: false,
-      default: undefined
-    },
-    reverseGeocode: {
-      type: Boolean as PropType<boolean>,
-      required: false,
-      default: false
-    },
-    standalone: {
-      type: Boolean as PropType<boolean>,
-      required: false,
-      default: false
-    },
-    collapsed: {
-      type: Boolean as PropType<boolean>,
-      required: false,
-      default: false
-    },
-    position: {
-      type: String as PropType<
-        'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
-      >,
-      default: 'top-right'
-    }
+interface Props {
+  countries?: string[] | null
+  markerOptions?: MarkerOptions | boolean
+  reverseGeocode?: boolean
+  standalone?: boolean
+  collapsed?: boolean
+  position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+}
+const props = withDefaults(defineProps<Props>(), {
+  countries: null,
+  markerOptions: undefined,
+  reverseGeocode: false,
+  standalone: false,
+  collapsed: false,
+  position: 'top-right'
+})
+
+interface Emits {
+  (e: 'result', result: GeocodeResult): void
+}
+const emit = defineEmits<Emits>()
+
+const geocodeControl = new MaplibreGeocoder(
+  {
+    forwardGeocode: forwardGeocode,
+    reverseGeocode: reverseGeocode
   },
-  emits: ['result'],
-  setup(props, { emit }) {
-    const geocodeControl = new MaplibreGeocoder(
-      {
-        forwardGeocode: forwardGeocode,
-        reverseGeocode: reverseGeocode
-      },
-      {
-        collapsed: props.collapsed,
-        countries: props.countries?.join(',') ?? undefined,
-        marker: props.markerOptions,
-        placeholder: 'Suchen',
-        showResultsWhileTyping: true,
-        maplibregl: maplibregl
-      }
-    )
-    const geocodeWrapper = ref<HTMLElement | null>(null)
-    geocodeControl.on('result', ({ result }: { result: GeocodeResult }) => {
-      emit('result', result)
-    })
+  {
+    collapsed: props.collapsed,
+    countries: props.countries?.join(',') ?? undefined,
+    marker: props.markerOptions,
+    placeholder: 'Suchen',
+    showResultsWhileTyping: true,
+    maplibregl: maplibregl
+  }
+)
+const geocodeWrapper = ref<HTMLElement | null>(null)
+geocodeControl.on('result', ({ result }: { result: GeocodeResult }) => {
+  emit('result', result)
+})
 
-    onMounted(() => {
-      if (!props.standalone) {
-        const map = inject(MapKey)
-        if (!map?.value) {
-          throw Error(
-            'Either mount the Geocoder as a child of Map or set standalone=true'
-          )
-        }
-        map?.value.addControl(geocodeControl, props.position)
-      } else {
-        geocodeControl.addTo(geocodeWrapper.value)
-      }
-    })
-
-    onUnmounted(() => {
-      if (!props.standalone) {
-        const map = inject(MapKey)
-        map?.value?.removeControl(geocodeControl)
-      }
-    })
-
-    return {
-      geocodeWrapper,
-      query: (input: string) => geocodeControl.query(input)
+onMounted(() => {
+  if (!props.standalone) {
+    const map = inject(MapKey)
+    if (!map?.value) {
+      throw Error(
+        'Either mount the Geocoder as a child of Map or set standalone=true'
+      )
     }
+    map?.value.addControl(geocodeControl, props.position)
+  } else {
+    geocodeControl.addTo(geocodeWrapper.value)
+  }
+})
+
+onUnmounted(() => {
+  if (!props.standalone) {
+    const map = inject(MapKey)
+    map?.value?.removeControl(geocodeControl)
   }
 })
 </script>
