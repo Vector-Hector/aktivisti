7663ae4fcd19263207f1c022c90d1fc15d6fdc97
Peter Strümpel <peter@ctrl.alt.coop>
2024-05-13 16:02:00 +0200

    CAC_FRONTEND-397 Migrate EventDetailPosterDetailMap

--- src/pages/event-map/detail/area/posters/detail/EventDetailPosterDetailMap.vue
+++ src/pages/event-map/detail/area/posters/detail/EventDetailPosterDetailMap.vue
@@ -1,24 +1,20 @@
+<script setup lang="ts">
+import { useEventDetailPosterMixin } from 'pages/event-map/detail/area/posters/EventDetailPosterMixin'
+import { inject, onMounted, onUpdated } from 'vue'
+import { MapKey } from 'src/types/keys'
+
+const { poster } = useEventDetailPosterMixin()
+const map = inject(MapKey)!
+
+onMounted(() => {
+  map.value?.panTo(poster.value.location)
+})
+
+onUpdated(() => {
+  map.value?.panTo(poster.value.location)
+})
+</script>
 <template>
   <span />
 </template>
-<script lang="ts">
-import { useEventDetailPosterMixin } from 'pages/event-map/detail/area/posters/EventDetailPosterMixin'
-import { defineComponent, inject } from 'vue'
-import { MapKey } from 'src/types/keys'
-
-export default defineComponent({
-  name: 'EventDetailPosterDetailMap',
-  setup() {
-    const { poster } = useEventDetailPosterMixin()
-    const map = inject(MapKey)!
-    return { map, poster }
-  },
-  created() {
-    this.map?.panTo(this.poster.location)
-  },
-  updated() {
-    this.map?.panTo(this.poster.location)
-  }
-})
-</script>
 <style></style>
