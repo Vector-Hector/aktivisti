0d1bf3211a310944ff222468a340a53127ce4efa
Peter Strümpel <peter@ctrl.alt.coop>
2024-05-13 16:02:00 +0200

    CAC_FRONTEND-397 Migrate PageLoadingSpinner

--- src/components/PageLoadingSpinner.vue
+++ src/components/PageLoadingSpinner.vue
@@ -1,19 +1,11 @@
+<script setup lang="ts">
+import { QSpinnerPuff } from 'quasar'
+</script>
 <template>
   <div class="loading-spinner-wrapper">
     <QSpinnerPuff class="loading-spinner" color="primary" />
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { QSpinnerPuff } from 'quasar'
-
-export default defineComponent({
-  name: 'PageLoadingSpinner',
-  components: {
-    QSpinnerPuff
-  }
-})
-</script>
 <style lang="scss" scoped>
 .loading-spinner-wrapper {
   align-items: center;
