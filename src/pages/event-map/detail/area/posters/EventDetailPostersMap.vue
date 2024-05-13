06541d0244a8c3b7ec28ad5bf9a6f298d24f6098
Peter Strümpel <peter@ctrl.alt.coop>
2024-05-13 16:02:00 +0200

    CAC_FRONTEND-397 Migrate EventDetailPostersMap

--- src/pages/event-map/detail/area/posters/EventDetailPostersMap.vue
+++ src/pages/event-map/detail/area/posters/EventDetailPostersMap.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import PosterMarkerLayer from 'src/map/PosterMarkerLayer.vue'
+import { useEventDetailPosterMixin } from 'pages/event-map/detail/area/posters/EventDetailPosterMixin'
+import AreaFeatureLayer from 'src/map/AreaFeatureLayer.vue'
+import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
+
+const { currentAreaFeature, activePosterIndex, postersInArea } =
+  useEventDetailStore()
+const { selectPoster } = useEventDetailPosterMixin()
+</script>
 <template>
   <AreaFeatureLayer
     v-if="currentAreaFeature"
@@ -7,30 +17,7 @@
     :posters="postersInArea"
     :editable="false"
     :active-poster-index="activePosterIndex"
-    @poster-click="selectPoster($event, true)"
+    @poster-click="(poster) => selectPoster(poster.id, true)"
   />
   <router-view name="map" />
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import PosterMarkerLayer from 'src/map/PosterMarkerLayer.vue'
-import { useEventDetailPosterMixin } from 'pages/event-map/detail/area/posters/EventDetailPosterMixin'
-import AreaFeatureLayer from 'src/map/AreaFeatureLayer.vue'
-import { useEventDetailStore } from 'pages/event-map/detail/EventDetailStoreMixin'
-
-export default defineComponent({
-  name: 'EventDetailPostersMap',
-  components: { AreaFeatureLayer, PosterMarkerLayer },
-  setup() {
-    const { currentAreaFeature, activePosterIndex, postersInArea } =
-      useEventDetailStore()
-    const { selectPoster } = useEventDetailPosterMixin()
-    return {
-      currentAreaFeature,
-      activePosterIndex,
-      postersInArea,
-      selectPoster
-    }
-  }
-})
-</script>
