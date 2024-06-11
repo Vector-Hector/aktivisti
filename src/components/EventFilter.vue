a59c41eea79a6041febe7638130fec44aa02c3ac
Peter Strümpel <peter@ctrl.alt.coop>
2024-06-11 16:55:50 +0200

    #17 Make activated counter depending on default values

--- src/components/EventFilter.vue
+++ src/components/EventFilter.vue
@@ -4,9 +4,9 @@ import { EventFilterParams } from 'src/api/params/EventFilterParams'
 import { CampaignDto } from 'src/api/model/CampaignDto'
 import { SubAssociationDto } from 'src/api/model/SubAssociationDto'
 import CollapsibleFilters from 'components/CollapsibleFilters.vue'
-import { EventStatus } from 'src/api/model/EventStatus'
 import EventFilterList from 'components/EventFilterList.vue'
 import { EventTypes } from 'src/api/model/EventTypes'
+import { DEFAULT_FILTER_PREFERENCES } from 'src/store/UserStore'
 
 interface Props {
   isCollapsible?: boolean
@@ -41,22 +41,25 @@ const emit = defineEmits<Emits>()
 
 const activatedFilterCount = computed(() => {
   let active = 0
-  if (props.filterParams.management_permission === true) {
+  if (
+    props.filterParams.management_permission !==
+    DEFAULT_FILTER_PREFERENCES.management_permission
+  ) {
     active++
   }
-  if (props.filterParams.is_owner !== undefined) {
+  if (props.filterParams.is_owner !== DEFAULT_FILTER_PREFERENCES.is_owner) {
     active++
   }
   if ((props.filterParams.sub_association?.length ?? 0) > 0) {
     active++
   }
-  if (props.filterParams.campaigns) {
+  if ((props.filterParams.campaigns?.length ?? 0) > 0) {
     active++
   }
-  if (props.filterParams.status === EventStatus.ENDED) {
+  if (props.filterParams.status !== DEFAULT_FILTER_PREFERENCES.status) {
     active++
   }
-  if (props.filterParams.event_type) {
+  if (props.filterParams.event_type !== DEFAULT_FILTER_PREFERENCES.eventType) {
     active++
   }
   return active
