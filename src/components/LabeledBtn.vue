82bd64ced40c06571d8aca4bc4728b85236d711b
Peter Strümpel <peter@ctrl.alt.coop>
2024-07-01 14:17:09 +0200

    Migrate file script setup

--- src/components/LabeledBtn.vue
+++ src/components/LabeledBtn.vue
@@ -1,33 +1,27 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { QBtn } from 'quasar'
+import { useBtnProps } from 'quasar/src/components/btn/use-btn.js'
+
+// TODO(peter) Use `interface Props` style
+const props = defineProps({
+  ...useBtnProps,
+  externalLabel: {
+    type: String as PropType<string>,
+    required: false
+  }
+})
+</script>
 <template>
   <div class="labeled-button">
     <slot name="btn">
       <QBtn v-bind="$props" />
     </slot>
     <div class="button-label">
-      {{ $props.externalLabel }}
+      {{ props.externalLabel }}
     </div>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { QBtn } from 'quasar'
-//@ts-ignore
-import { useBtnProps } from 'quasar/src/components/btn/use-btn.js'
-
-export default defineComponent({
-  name: 'LabeledBtn',
-  props: {
-    ...useBtnProps,
-    externalLabel: {
-      type: String as PropType<string>,
-      required: false
-    }
-  },
-  components: {
-    QBtn
-  }
-})
-</script>
 <style lang="scss" scoped>
 .labeled-button {
   text-align: center;
