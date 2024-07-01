a86bac1df55e2f4cbb8013675828b0e1789ddc1e
Peter Strümpel <peter@ctrl.alt.coop>
2024-07-01 14:37:38 +0200

    #12 Add notification badge to LabeledBtn component
    - **Props Update**:
      - Added `notificationCount` prop to specify the number of notifications.
      - Added `hasNotification` prop to indicate if there is a notification.
    
    - **Template Changes**:
      - Wrapped the button slot in a `button-container` div.
      - Added a `notification-badge` div to show notification count or indicator based on the new props.
    
    - **Styles Update**:
      - Styled `.button-container` to position the notification badge relative to the button.
      - Styled `.notification-badge` to appear as a red circle with white text, positioned in the top-right corner of the button.

--- src/components/LabeledBtn.vue
+++ src/components/LabeledBtn.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { PropType } from 'vue'
+import { PropType, computed } from 'vue'
 import { QBtn } from 'quasar'
 import { useBtnProps } from 'quasar/src/components/btn/use-btn.js'
 
@@ -9,16 +9,49 @@ const props = defineProps({
   externalLabel: {
     type: String as PropType<string>,
     required: false
+  },
+  notificationCount: {
+    type: Number,
+    default: 0
+  },
+  hasNotification: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const notificationStyle = computed(() => {
+  if (props.notificationCount > 0) {
+    return {
+      top: '-5px',
+      right: '-5px',
+      width: '20px',
+      height: '20px'
+    }
+  } else {
+    // Default style or when notification is not needed
+    return {}
   }
 })
 </script>
 <template>
   <div class="labeled-button">
-    <slot name="btn">
-      <QBtn v-bind="$props" />
-    </slot>
+    <div class="button-container">
+      <slot name="btn">
+        <QBtn v-bind="$props" />
+      </slot>
+      <div
+        v-if="props.notificationCount > 0 || props.hasNotification"
+        class="notification-badge"
+        :style="notificationStyle"
+      >
+        <span v-if="props.notificationCount">{{
+          props.notificationCount
+        }}</span>
+      </div>
+    </div>
     <div class="button-label">
-      {{ props.externalLabel }}
+      {{ $props.externalLabel }}
     </div>
   </div>
 </template>
@@ -26,5 +59,28 @@ const props = defineProps({
 .labeled-button {
   text-align: center;
   flex-direction: column;
+
+  .button-container {
+    position: relative;
+    display: inline-block;
+  }
+
+  .notification-badge {
+    position: absolute;
+    top: 2px;
+    right: -1px;
+    background-color: red;
+    color: white;
+    border-radius: 50%;
+    width: 10px;
+    height: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    // Using the same shadow as .q-btn:before
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14),
+      0 3px 1px -2px rgba(0, 0, 0, 0.12);
+  }
 }
 </style>
