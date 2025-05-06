8f93704e245dba70a0c53c7f1e2e833775366e39
Peter Strümpel <peter@ctrl.alt.coop>
2025-05-06 11:12:56 +0200

    Add fixme for internationalizing in beforeRoute
    At the moment the i18n t method isn't working in the workaround with
    the composition api and beforeRoute

--- src/pages/EditEvent.vue
+++ src/pages/EditEvent.vue
@@ -1,3 +1,5 @@
+<!--FIXME(peter) 2023/12/12 The composition API doesn't support `beforeRouteEnter` so far so this a workaround
+      see https://github.com/vuejs/rfcs/discussions/302-->
 <script lang="ts">
 export interface StepControls {
   isLastStep: ComputedRef<boolean>
@@ -10,6 +12,7 @@ export default {
     if (!userStore.hasAtLeastOneManagePermission()) {
       ErrorBus.emit(
         NOT_AUTHORIZED,
+        // FIXME(peter) Due to the workaround for the composition API, we need to use a string here for now, but needs to be internationalized in future
         'Um eine Aktion zu erstellen benötigst du eine Koordinator*innenberechtigung'
       )
       next({ name: 'login' })
