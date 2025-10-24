59f5e2bf2c1789314a37cd7cc61f339e517bc93f
Peter Strümpel <peter@ctrl.alt.coop>
2025-10-24 16:22:55 +0200

    Increase opiacity of event areas

--- src/map/AreaFeatureLayer.vue
+++ src/map/AreaFeatureLayer.vue
@@ -76,7 +76,7 @@ onMounted(async () => {
           IS_COMPLETED_COLOR,
           getColorFromPropertiesWithDefault('#000', 'color')
         ],
-        'fill-opacity': 0.1
+        'fill-opacity': 0.35
       }
     },
     firstSymbolId
