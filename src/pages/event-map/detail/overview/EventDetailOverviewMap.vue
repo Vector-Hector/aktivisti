23de126369aa69534ced9d704a9167438bc8ff64
Peter Strümpel <peter@ctrl.alt.coop>
2025-10-27 13:45:04 +0100

    Algin imported component name to filename
    This is done to make searches more easier and the code more
    comprehensible

--- src/pages/event-map/detail/overview/EventDetailOverviewMap.vue
+++ src/pages/event-map/detail/overview/EventDetailOverviewMap.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed, onMounted, watch } from 'vue'
-import FeatureLayer from 'src/map/AreaFeatureLayer.vue'
+import AreaFeatureLayer from 'src/map/AreaFeatureLayer.vue'
 import { BBox2d } from '@turf/helpers/dist/js/lib/geojson'
 import { useInjectMapMixin } from 'src/pages/event-detail/InjectMapMixin'
 import EventMarker from 'components/EventMarker.vue'
@@ -31,7 +31,7 @@ watch(
 
 <template>
   <EventMarker v-if="eventStore.event?.location" :event="eventStore.event" />
-  <FeatureLayer :features="eventStore.areaFeatures" />
+  <AreaFeatureLayer :features="eventStore.areaFeatures" />
 </template>
 
 <style lang="scss" scoped></style>
