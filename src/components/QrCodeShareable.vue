2ea1ecfcad06f5cd86ed97a806aa832b809f1472
Nils Kuekenshoener <nils@ctrl.alt.coop>
2025-11-03 17:52:46 +0100

    #106 Create a Qr-code Shareable component This component uses the qr-code component and adds a download button and a container

---
+++ src/components/QrCodeShareable.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import QrCode from './QrCode.vue'
+import { QBtn } from 'quasar'
+
+interface Props {
+  url: string
+  name: string
+}
+
+const props = defineProps<Props>()
+const qrRef = ref<InstanceType<typeof QrCode> | null>(null)
+function downloadQrCode(): void {
+  qrRef.value.downloadQrCode()
+}
+</script>
+<template>
+  <div class="qr-code-container">
+    <QrCode ref="qrRef" :name="props.name" :url="props.url"></QrCode>
+    <QBtn class="full-width" color="primary" @click="downloadQrCode()"
+      >Download</QBtn
+    >
+  </div>
+</template>
+
+<style scoped>
+.qr-code-container {
+  background: transparent;
+  padding: 1rem;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  margin: 1rem;
+  padding: auto;
+  .q-btn {
+    margin-top: 1rem;
+  }
+}
+</style>
