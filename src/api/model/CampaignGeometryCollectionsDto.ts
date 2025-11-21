28d663eab2288abe3503d3aac4934bd9c989178b
Nils Kuekenshoener <nils@ctrl.alt.coop>
2025-11-21 16:08:18 +0100

    #127 Add event_types to dto

--- src/api/model/CampaignGeometryCollectionsDto.ts
+++ src/api/model/CampaignGeometryCollectionsDto.ts
@@ -1,7 +1,10 @@
+import { EventTypes } from './EventTypes'
+
 export interface CampaignGeometryCollectionsDto {
   name: string
   id: number
   campaign: number
   color: string
+  event_types: EventTypes[]
   import_errors: string[]
 }
